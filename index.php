10ca79512a8f7f347e899de10e86240125f31cff
Petra Gianni Sanhueza Maffezzoli <142603915+P-Maffezzoli@users.noreply.github.com>
2024-04-02 08:09:46 -0300

    inserindo uma logo ao site

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="imgs/senai-logo.jpg">
     <title>Sistema Logistica</title>
     <link rel="stylesheet" href="estilo/style.css">
 </head>
