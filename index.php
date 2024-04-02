f06bc966be00ff6dfcecd45428af745a29db9dbb
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-04-02 07:53:26 -0300

    <!DOCTYPE html> <html lang="en"> <head>     <meta charset="UTF-8">     <meta name="viewport" content="width=device-width, initial-scale=1.0">     <title>Document</title> </head> <body>     <p>André Gatão</p> </body> </html>

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema Logistica</title>
+    <link rel="stylesheet" href="estilo/style.css">
 </head>
 <body>
     
+        
 </body>
 </html>
