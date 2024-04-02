e00df8495e52bb69698b8e91ff0527eed19287da
Petra Gianni Sanhueza Maffezzoli <142603915+P-Maffezzoli@users.noreply.github.com>
2024-04-02 08:27:58 -0300

    nova imagem para o ícone do site

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="imgs/senai-logo.jpg">
+    <link rel="icon" href="imgs">
     <title>Sistema Logistica</title>
     <link rel="stylesheet" href="estilo/style.css">
 </head>
