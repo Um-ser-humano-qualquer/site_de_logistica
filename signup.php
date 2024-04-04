07b6e86fc86149ef462931caf457f17a2573bc06
Petra Gianni Sanhueza Maffezzoli <142603915+P-Maffezzoli@users.noreply.github.com>
2024-04-04 09:28:28 -0300

    Atualização da tela de cadastro

---
+++ signup.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="imgs/senai-logo.png">
+    <title>Sistema Logistica</title>
+    <link rel="stylesheet" href="estilo/style.css">
+</head>
+<body>
+    <div class="">
+			<form method="post" action="verificaLogin.php" id="formlogin" name="formlogin" >
+
+				<input type="text" placeholder='Email' name="emailUsuario" id="emailUsuario" size="20"><br />
+				
+				<input type="password" placeholder='Senha' name="senha" id="senha" size="20">
+				<br>
+				<center>
+					<input type="submit" value="Cadastrar"  />
+				</center>
+			</form>
+		</div>
+
+	<center>
+		<a href="index.php">Já possuo cadastro.</a>
+	</center>
+        
+</body>
+</html>
