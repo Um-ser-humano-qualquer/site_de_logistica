88431f3fb9092b101e8160e65399fcf27e6dd3c0
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-04-09 08:06:46 -0300

    algumas alterações na tela de cadastro

--- signup.php
+++ signup.php
@@ -8,35 +8,18 @@
             <center>
 			<div class="divLogin">
 			<form method="post" action="cadastro.php" id="formcadastro" name="formcadastro" >
-				<h2>Registre um filme:</h2>
-				<input type="text" name="titulo" id="titulo" size="20" class="caixaTexto" placeholder="título"><br />
+				<h2>Cadastro:</h2>
+				<input type="text" name="nome" id="nome" size="20" class="caixaTexto" placeholder="nome"><br />
 				<br>
-				<input type="text" name="duracao" id="duracao" size="20" class="caixaTexto" placeholder="duração"><br />
+				<input type="text" name="email" id="email" size="20" class="caixaTexto" placeholder="email"><br />
 				<br>
-				<input type="text" name="classificacao" id="classificacao" size="20" class="caixaTexto" placeholder="classificação indicativa"><br />
+				<input type="text" name="senha" id="senha" size="20" class="caixaTexto" placeholder="senha"><br />
 				<br>
-				<input type="text" name="sinopse" id="sinopse" size="20" class="caixaTexto" placeholder="sinopse"><br/>
-                <br>
-				<input type="text" name="lancamento" id="lancamento" size="20" class="caixaTexto" placeholder="lançamento"><br/>
-                <br>
-				<input type="text" name="atores" id="atores" size="20" class="caixaTexto" placeholder="atores principais"><br/>
-                <br>
-				<input type="text" name="diretor" id="diretor" size="20" class="caixaTexto" placeholder="diretor"><br/>
-                <br>
-				<input type="submit" value="registrar" class="botao"/>
-                <br>
                 <a href="index.php" class="link">Já possui login? Clique aqui!</a>
 			</form>
 			</div>
             <p></p>
             </center>
             
-            <div>
-            <form method="post" action="pesquisa.php" id="formpesquisa" name="formpesquisa" >
-            <input type="text" name="pesquisa" id="pesquisa" size="20" class="caixaTexto2" placeholder="buscar">
-            <br>
-            <p></p>
-			<input type="submit" value="pesquisar" class="botao2"/>
-            </div>
     </body>
 </html>
