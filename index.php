a6fec834e63af4da179068f85636c4640fc623f1
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-04-11 12:32:39 -0300

    modificações no div do index

--- index.php
+++ index.php
@@ -6,20 +6,23 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     </head>
     <body class="fundo" background="img/fundo_login.png">	
-        
+
             <center>
-			<div class="divLogin">
+			<div class="divLoginL">
 			<form method="post" action="loginAluno.php" id="formcadastro" name="formcadastro">
 				<h1 >Login de alunos:</h1>
-				
+                <br>
 				<input type="text" name="email" id="email" size="20" class="caixaTexto" placeholder="email"><br/>
 				<br>
+                <br>
 				<input type="text" name="senha" id="senha" size="20" class="caixaTexto" placeholder="senha"><br/>
 				<br>
-                <a href="signupProf.php" class="link" >Você é um professor? Clique aqui!</a>
+                <br>
+                <input type="submit" value="registrar" class="botao"/>
+                
 			</form>
 			</div>
-            <p></p>
+            <a href="signupProf.php" class="link">Você é um professor? Clique aqui!</a>
             </center>
             
     </body>
