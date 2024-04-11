8a3dbc0b593847792af224f3050320cadb3afe80
Petra Gianni Sanhueza Maffezzoli <142603915+P-Maffezzoli@users.noreply.github.com>
2024-04-11 09:23:58 -0300

    mudanças de css

--- index.php
+++ index.php
@@ -9,25 +9,22 @@
 </head>
 <body>
 
-	<center>
-    <div class="divlogin">
-		
-            
-            <input type="text" placeholder='Email' name="emailUsuario" class="caixaTexto" id="emailUsuario" size="20"><br />
-				
-				<input type="password" placeholder='Senha' name="senha" class="caixaTexto" id="senha" size="20">
-				<br>
-				<center>
+	<div class= "container">
+    <div class= "divLogin">
+         <input type="text" placeholder='Email' name="emailUsuario" class="caixaTexto" id="emailUsuario" size="20">
+	 	<br>
+			<input type="password" placeholder='Senha' name="senha" class="caixaTexto" id="senha" size="20">
+		<br>
+			<center>
 					<input type="submit" value="LOGAR" class="botao">
 				</center>
 			</form>
 			<br>
-			<input type="checkbox" id="bancoProf" name="bancoProf" checked />
-    	<label for="signupProfOk"> Fazer login como professor </label>
+			<input type="checkbox" id="bancoProf" name="bancoProf"/>
+    	<label for="signupProfTrue"> Fazer login como professor? </label>
 		</div>   
-	
-		<a href="signup.php">Não possui uma conta? Clique aqui!</a>
-	</center>
+	</div>
+		
         
 </body>
 </html>
