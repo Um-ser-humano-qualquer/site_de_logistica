fcba6b22a8051caacec81854cabeb5e17f1530b3
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-04-16 09:33:44 -0300

    MUITAS MUDANÇAS NO CSS

--- index.php
+++ index.php
@@ -3,11 +3,49 @@
         <title>Logística Senai</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <style>
+    .divLoginL{
+        height: auto;
+        top: 50%;
+        left: 30%;
+        transform: translate(-50% , -50%);
+        position:fixed;
+        padding: 72px 20px;
+        background-color: #237deb;
+        border-radius:100px;
+        box-shadow: 15px 17px 20px 0px #0000007f;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+        color: rgb(255, 255, 255);
+    
+        }
+    .senaiLogo{
+        position: fixed;
+        top: -11%;
+        left: 1%;
+        }
+        
+    .botaoo{
+        background-color: #1b68c4;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+        width: 9rem;
+        height: 2rem;   
+        border-radius: 15px;
+        color: white;
+        border-block-color: revert;
+        border-block-start-width: initial;
+        border-block-end: none;
+        border-left-color: #ffffff00;
+        border-right-color: #ffffff00;
+        border-top-color: #ffffff00;
+        cursor: pointer;
+        }
+
+    </style>
     </head>
-    <body class="fundo" background="img/fundo_login.png">	
+    <body class="fundo" background="img/fundo_alunoLogin.png">	
 
             <center>
+            <img src="img/senai-logo.png" class="senaiLogo" alt="Image" height="420" width="420">
 			<div class="divLoginL">
 			<form method="post" action="loginAluno.php" id="formcadastro" name="formcadastro">
 				<h1 >Login de alunos:</h1>
@@ -18,7 +56,7 @@
 				<input type="text" name="senha" id="senha" size="20" class="caixaTexto" placeholder="senha"><br/>
 				<br>
                 <br>
-                <input type="submit" value="registrar" class="botao"/>
+                <input type="submit" value="Entrar" class="botaoo"/>
                 
 			</form>
 			</div>
