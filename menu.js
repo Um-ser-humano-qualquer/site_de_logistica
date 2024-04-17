6d02086aa6f97be1f4ec6ed961061fce0762863c
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-04-17 12:26:49 -0300

    Ínicio de codificação da página inicial (página do aluno)

---
+++ menu.js
@@ -0,0 +1,20 @@
+
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectlink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectlink)
+)
+
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
--- pagina1Aluno.php
+++ pagina1Aluno.php
@@ -4,91 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial</title>
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@300&display=swap');
-
-    *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-family: 'Outfit', sans-serif;
-    }
-
-    body{
-        height: 900vh;
-    }
-
-    nav.menu-lateral{
-        width: 70px;
-        height: 100%;
-        background-color: #202020;
-        padding: 40px o 40px 1%;
-        box-shadow: 3px 0 0 #FF0077;
-
-        position: fixed;
-        top: 0%;
-        left: 0%;
-        overflow: hidden;
-        transition: .2s;
-    }
-
-    nav.menu-lateral:hover{
-        width: 300px;
-    }
-
-    .btn-expandir{
-        width: 100%;
-        padding-left: 10px;
-    }
-
-    .btn-expandir > i{
-        color: #fff;
-        font-size: 24px;
-        cursor: pointer;
-    }
-
-    ul{
-        height: 100%;
-        list-style-type: none;
-    }
-
-    ul li.item-menu{
-        transition: .5s;
-    }
-
-    ul li.item-menu:hover{
-        background: #FF0077;
-    }
-    
-    ul li.item-menu a{
-        color: #fff;
-        text-decoration: none;
-        font-size: 20px;
-        padding: 20px 4%;
-        display: flex;
-        margin-bottom: 20px;
-        line-height: 40px;
-    }
-
-    ul li.item-menu a .txt-link{
-        margin-left: 40px;
-
-    }
-    ul li.item-menu a .icon > i{
-        font-size: 30px;
-        margin-left: 15px;
-    }
-
-</style>
 </head>
 <body>
 
     <nav class="menu-lateral">
 
         <div class="btn-expandir">
-            <i class="bi bi-list"></i>
+            <i class="bi bi-list" id="btn-exp"></i>
         </div>
 
         <ul>
@@ -120,8 +44,6 @@
 
     </nav>
 
-    <script src="menu.js">
-
-    </script>
+    <script src="menu.js"></script>
 </body>
 </html>
