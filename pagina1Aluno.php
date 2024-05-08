c6705965e00846df57adb90818090c4fb04012f7
Ruthy Hellen Dias <142604700+R2618@users.noreply.github.com>
2024-05-08 12:34:14 -0300

    Mudanças no style.css, adição do fundo na pagina1aluno

--- pagina1Aluno.php
+++ pagina1Aluno.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial</title>
     <link rel="stylesheet" href="styleDoMenu.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     
 </head>
-<body class= "fundinho">
+<body class= "fundinho2">
 
     <nav class="menu-lateral">
 
