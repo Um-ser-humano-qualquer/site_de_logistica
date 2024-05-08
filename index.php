cc8c3b834ab0aacb3333386f1583544bd8885d5a
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-05-07 22:23:57 -0300

    Reestruturação do código do Index

--- index.php
+++ index.php
@@ -4,38 +4,40 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="css/styleDoBotao.css">
-        <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <link rel="stylesheet" href="styleDoLogin.css">
     </head>
 
-    <body class="fundo" background="img/login_fundu.png">
-
-            <center>
-			<div class="divLoginL" class="text-box">
-            <div class="divLoginLL">
-			<form method="post" action="processoLoginAluno.php" id="formcadastro" name="formcadastro">
-				<h1>Login de alunos:</h1>
-                <br>
-				<input type="text" name="emailAluno" id="emailAluno" size="20" class="caixaTexto" placeholder="email"><br/>
-				<br>
-                <br>
-				<input type="password" name="senhaAluno" id="senhaAluno" size="20" class="caixaTexto" placeholder="senha"><br/>
-				<br>
-                <br>
-                <button class="btn btn-1 btn-sep icon-info">acessar</button>
-                <br>
-                <a href="loginProf.php" class="linkL">Você é um professor? Clique aqui!</a>
-			</form>
-            </div>
-			</div>
-            <nav class="transicao">
-                <div class="btn-expandir">
-                <img src="img/senai-logo.png" class="senaiLogoL" alt="Image" height="420" width="420">
+    <body>
+        <div class="container">
+            <div class="content first-content">
+                <div class="second-column">
+                    <h2 class="title title-second">login de Aluno</h2>
+                    <br>
+                    <form action="post" action="processoLoginAluno.php" class="form">
+                        <input type="email" name="emailAluno" id="emailAluno" placeholder="email">
+                        <br>
+                        <input type="password" name="senhaAluno" id="senhaAluno" placeholder="senha">
+                        <br>
+                        <button class="btn-second btn-right">acessar</button>
+                    </form>
+                </div> 
+                <div class="first-column">
+                    <img src="img/senai-logo.png" class="senaiLogo">
+                    <a href="" class="linkR">Você é um professor? Clique aqui!</a>
                 </div>
-            </nav>
-            </center>
-            
+            </div>
+            <div class="content second-content">
+                <div class="first-column"></div>
+                    <h2 class="title">Você é um Aluno?</h2>
+                    <button class="btn">clique aqui!</button>
+                <div class="second-column">
+                    <h2 class="title">login de Professor</h2>
+                    <form action="post" action="processoLoginProf.php">
+                        <input type="email" name="emailAluno" id="emailAluno" placeholder="email">
+                        <input type="password" name="senhaAluno" id="senhaAluno" placeholder="senha">
+                        <button class="btn">acessar</button>
+                    </form>
+                </div> 
+            </div> 
     </body>
 </html>
