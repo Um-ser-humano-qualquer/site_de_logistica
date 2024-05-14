8ea12e6b3688ee19c40948df8207df94bf21f7d4
Petra Gianni Sanhueza Maffezzoli <142603915+P-Maffezzoli@users.noreply.github.com>
2024-05-14 12:22:55 -0300

    ainda tentando fazer o banco de dados(ainda não funciona)

--- processoAlunopg2.php
+++ processoAlunopg2.php
@@ -6,34 +6,35 @@ $user = "root";
 $password = "root";
 $database = "sa_logistica";
 
-$connnect = new mysqli($hostname, $user, $password,$database);
+$connect = new mysqli($hostname, $user, $password,$database);
 
-if ($connnect -> connect_errno) {
- echo "Failed to connect to MySQL: " . $connnect -> connect_error;
+if ($connect -> connect_errno) {
+ echo "Failed to connect to MySQL: " . $connect -> connect_error;
  exit();
  } else {
- // Evita caracteres especiais (SQL Inject)
- $placa_caminhao = $connnect -> real_escape_string($_POST['Placa do caminhão']);
- $nome_motorista = $connnect -> real_escape_string($_POST['Nome do motorista']);
- $container = $connnect -> real_escape_string($_POST['Container']);
- $navio = $connnect -> real_escape_string($_POST['Navio']);
- $cliente = $connnect -> real_escape_string($_POST['Cliente']);
- $tipo = $connnect -> real_escape_string($_POST['Tipo']);
- $lacre = $connnect -> real_escape_string($_POST['Lacre']);
- $lacre_sif = $connnect -> real_escape_string($_POST['LacreSIF']);
- $temperatura = $connnect -> real_escape_string($_POST['Temperatura']);
- $IMO = $connnect -> real_escape_string($_POST['IMO']);
- $n_onu = $connnect -> real_escape_string($_POST['N°ONU']);
+ 
+   $placa_caminhao = $connect->real_escape_string($_POST['placa_caminhao']);
+
+ $nome_motorista = $connect -> real_escape_string($_POST['nome_motorista']);
+ $container = $connect -> real_escape_string($_POST['container']);
+ $navio = $connect -> real_escape_string($_POST['navio']);
+ $cliente = $connect -> real_escape_string($_POST['cliente']);
+ $tipo = $connect -> real_escape_string($_POST['tipo']);
+ $lacre = $connect -> real_escape_string($_POST['lacre']);
+ $lacre_sif = $connect -> real_escape_string($_POST['lacreSIF']);
+ $temperatura = $connect -> real_escape_string($_POST['temperatura']);
+ $IMO = $connect -> real_escape_string($_POST['IMO']);
+ $n_onu = $connect -> real_escape_string($_POST['n_onu']);
 
  
- $sql="INSERT INTO `conteiner`
+ $sql="INSERT INTO `aluno_conteiner`
  (`placa_caminhao`, `nome_motorista`, `container`, `navio`, `cliente`, `tipo`, `lacre`, `lacre_sif`, `temperatura`, `IMO`, `n_onu`)
  VALUES
- ('".$placa_caminhao."', '".$nome_motorista."', '".$container."', '".$navio."', '".$tipo."', '".$lacre."', '".$lacre_sif."', '".$temperatura."', '".$IMO."', '".$n_onu."')";
+ ('".$placa_caminhao."', '".$nome_motorista."', '".$container."', '".$navio."',  '".$cliente."', '".$tipo."', '".$lacre."', '".$lacre_sif."', '".$temperatura."', '".$IMO."', '".$n_onu."')";
  
- $resultado = $connnect->query($sql);
+ $resultado = $connect->query($sql);
  
- $connnect -> close();
+ $connect -> close();
     header('location: pagina2Aluno.php', true, 301);                
  exit();
  }
