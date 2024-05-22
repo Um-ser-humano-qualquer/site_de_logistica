fec16d56d72ef4fa45e525b7bf78998b96841384
Ruthy Hellen Dias <142604700+R2618@users.noreply.github.com>
2024-05-22 11:40:47 -0300

    modificações nas paginas 4 e 3, criação do stylepgA.css

--- pagina3Aluno.php
+++ pagina3Aluno.php
@@ -5,7 +5,7 @@
 <meta charset="UTF-8">
 
     <meta charset="UTF-8">
-<meta charset="UTF-8">
+<meta charset="UTF-8"> <!-- style's css-->
 
     <link rel="stylesheet" href="styleDoMenu.css">
     <meta charset="UTF-8">
@@ -22,196 +22,198 @@
 
 <body class="fundinhoA">
 
- <nav> <!-- barra superior -->
-    <nav>
+    <nav> <!-- barra superior -->
+        <nav>
 
-        <div class="barra-superior">
-            <a href="profile.php">
-                <i class="bi bi-person-circle perfil"></i>
-            </a>
-        </div>
-    </nav>
-    
-<div class="divLognV"> <!-- tabela 1 -->
-        <div class="divLogin divLogin-lefts">  
-        <center><h2 class="texto">Operação em Aberto</h2></center> 
-        <br>
-        <br>  
-            <table border ="1"> 
-                    <tr>
-                        <td><input type="text" name="tipo" size="12" class="caixaText" placeholder="Tipo"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="tipo" size="12" class="caixaText" placeholder="Tipo"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="tipo" size="12" class="caixaText" placeholder="Tipo"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="tipo" size="12" class="caixaText" placeholder="Tipo"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="tipo" size="12" class="caixaText" placeholder="Tipo"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="tipo" size="12" class="caixaText" placeholder="Tipo"><br/></td>
-                    </tr>
-            </table>
+            <div class="barra-superior">
+                <a href="profile.php">
+                    <i class="bi bi-person-circle perfil"></i>
+                </a>
+            </div>
+        </nav>
+        
+    <div class="divLognV"> <!-- tabela 1 -->
+     <div class="divLogin divLogin-lefts">  
+            <center><h2 class="texto">Operação em Aberto</h2></center> 
             <br>
-
-  <div class="divLogin divLogin-Rights"> <!-- tabela 2 -->
-        <center><h2 class="texto">UN</h2></center> 
-        <br> 
-        <br>   
-            <table border ="1"> 
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="RL"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="RL"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="RL"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="RL"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="RL"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="RL"><br/></td>
-                    </tr>
-            </table>
-        </div>
-    </div>
- 
- <div class="divLogin divLogin-Righ"> <!-- tabela 3 -->
-        <center><h2 class="texto">QTD</h2></center> 
-        <br> 
-        <br>   
-            <table border ="1"> 
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="Núm"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="Núm"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="Núm"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="Núm"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="Núm"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="Núm"><br/></td>
-                    </tr>
-            </table>
+            <br>  
+                <table border ="1"> 
+                        <tr>
+                            <td><input type="text" name="tipo" size="12" class="caixaText" placeholder="Tipo"><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="tipo" size="12" class="caixaText" placeholder="Tipo"><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="tipo" size="12" class="caixaText" placeholder="Tipo"><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="tipo" size="12" class="caixaText" placeholder="Tipo"><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="tipo" size="12" class="caixaText" placeholder="Tipo"><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="tipo" size="12" class="caixaText" placeholder="Tipo"><br/></td>
+                        </tr>
+                </table>
+                <br>
             </div>
         </div>
 
- <div class="divLogin divLogin-lef"> <!-- tabela 4 -->
-        <center><h2 class="texto">Posição</h2></center> 
-        <br> 
-        <br>   
-            <table border ="1"> 
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="Pose"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="Pose"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="Pose"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="Pose"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="Pose"><br/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="o" size="1" class="caixaText2" placeholder="Pose"><br/></td>
-                    </tr>
-            </table>
+    <div class="divLogin divLogin-Rights"> <!-- tabela 2 -->
+            <center><h2 class="texto">UN</h2></center> 
+            <br> 
+            <br>   
+                <table border ="1"> 
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                </table>
             </div>
         </div>
     
- <div class="divLogin divLogin-let"> <!-- tabela 5 -->  
+    <div class="divLogin divLogin-Righ"> <!-- tabela 3 -->
+            <center><h2 class="texto">QTD</h2></center> 
+            <br> 
+            <br>   
+                <table border ="1"> 
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                </table>
+                </div>
+            </div>
 
-                    <br><center><input type="submit" value="enviar" class="botao"/></center><p></p>
-                    <br><center><input type="submit" value="enviar" class="botao"/></center><p></p>
-                    <br><center><input type="submit" value="enviar" class="botao"/></center><p></p>
-                    <br><center><input type="submit" value="enviar" class="botao"/></center><p></p>
-                    <br><center><input type="submit" value="enviar" class="botao"/></center><p></p>
-                    <br><center><input type="submit" value="enviar" class="botao"/></center><p></p>
+    <div class="divLogin divLogin-lef"> <!-- tabela 4 -->
+            <center><h2 class="texto">Posição</h2></center> 
+            <br> 
+            <br>   
+                <table border ="1"> 
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="o" size="1" class="caixaText2" placeholder=""><br/></td>
+                        </tr>
+                </table>
+                </div>
+            </div>
+        
+    <div class="divLogin divLogin-let"> <!-- tabela 5 -->  
+
+                        <br><center><input type="submit" value="enviar" class="botao"/></center><p></p>
+                        <br><center><input type="submit" value="enviar" class="botao"/></center><p></p>
+                        <br><center><input type="submit" value="enviar" class="botao"/></center><p></p>
+                        <br><center><input type="submit" value="enviar" class="botao"/></center><p></p>
+                        <br><center><input type="submit" value="enviar" class="botao"/></center><p></p>
+                        <br><center><input type="submit" value="enviar" class="botao"/></center><p></p>
+            </div>
+
+        <nav class="menu-lateral">
+
+        <div class="btn-expandir"> <!--barra de menu 1 -->
+            <i class="bi bi-list" id="btn-exp"></i>
         </div>
 
-    <nav class="menu-lateral">
+    <ul> <!-- menu -->
+        <li class="item-menu">
+            <a href="pagina1Aluno.php">
+                <span class="icon"><i class="bi bi-house"></i></span>
+                <span class="txt-link">Home</span>
+            </a>
+        </li>
+        <li class="item-menu">
+            <a href="pagina2Aluno.php">
+                <span class="icon"><i class="bi bi-columns-gap"></i></span>
+                <span class="txt-link">Recebimento</span>
+            </a>
+        </li>
+        <li class="item-menu ativo">
+            <a href="pagina3aluno.php">
+                <span class="icon"><i class="bi bi-signpost-2-fill"></i></span>
+                <span class="txt-link">Movimentação</span>
+            </a>
+        </li>
+        <li class="item-menu">
+            <a href="pagina4Aluno.php">
+                <span class="icon"><i class="bi bi-box-seam-fill"></i></span>
+                <span class="txt-link">Estoque</span>
+            </a>
+        </li>
+        <li class="item-menu">
+            <a href="">
+                <span class="icon"><i class="bi bi-stack"></i></span>
+                <span class="txt-link">Picking</span>
+            </a>
+        </li>
+        <li class="item-menu">
+            <a href="">
+                <span class="icon"><i class="bi bi-clipboard-data-fill"></i></span>
+                <span class="txt-link">Expedição</span>
+            </a>
+        </li>
+        <li class="item-menu">
+            <a href="">
+                <span class="icon"><i class="bi bi-file-earmark-text-fill"></i></span>
+                <span class="txt-link">Relatórios</span>
+            </a>
+        </li>
+        <li class="item-menu">
+            <a href="">
+                <span class="icon"><i class="bi bi-pin-map-fill"></i></span>
+                <span class="txt-link">Controle</span>
+            </a>
+        </li>
+    </ul>
 
-    <div class="btn-expandir"> <!--barra de menu 1 -->
-        <i class="bi bi-list" id="btn-exp"></i>
-    </div>
+    </nav>
 
-<ul> <!-- menu -->
-    <li class="item-menu">
-        <a href="pagina1Aluno.php">
-            <span class="icon"><i class="bi bi-house"></i></span>
-            <span class="txt-link">Home</span>
-        </a>
-    </li>
-    <li class="item-menu">
-        <a href="pagina2Aluno.php">
-            <span class="icon"><i class="bi bi-columns-gap"></i></span>
-            <span class="txt-link">Recebimento</span>
-        </a>
-    </li>
-    <li class="item-menu ativo">
-        <a href="pagina3aluno.php">
-            <span class="icon"><i class="bi bi-signpost-2-fill"></i></span>
-            <span class="txt-link">Movimentação</span>
-        </a>
-    </li>
-    <li class="item-menu">
-        <a href="pagina4Aluno.php">
-            <span class="icon"><i class="bi bi-box-seam-fill"></i></span>
-            <span class="txt-link">Estoque</span>
-        </a>
-    </li>
-    <li class="item-menu">
-        <a href="">
-            <span class="icon"><i class="bi bi-stack"></i></span>
-            <span class="txt-link">Picking</span>
-        </a>
-    </li>
-    <li class="item-menu">
-        <a href="">
-            <span class="icon"><i class="bi bi-clipboard-data-fill"></i></span>
-            <span class="txt-link">Expedição</span>
-        </a>
-    </li>
-    <li class="item-menu">
-        <a href="">
-            <span class="icon"><i class="bi bi-file-earmark-text-fill"></i></span>
-            <span class="txt-link">Relatórios</span>
-        </a>
-    </li>
-    <li class="item-menu">
-        <a href="">
-            <span class="icon"><i class="bi bi-pin-map-fill"></i></span>
-            <span class="txt-link">Controle</span>
-        </a>
-    </li>
-</ul>
+    <script src="js/menu.js"></script> 
 
-</nav>
-
-<script src="js/menu.js"></script> 
-
-</center>
+    </center>
 
 </body>
 </html>
--- pagina4Aluno.php
+++ pagina4Aluno.php
@@ -10,18 +10,18 @@
     <link rel="stylesheet" href="Menu.css">
     <link rel="stylesheet" href="styleDoBotaoGeral.css">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="stylepgC.css">
+    <link rel="stylesheet" href="stylepgA.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-    <title>Estoque</title>
+    <title>Carga</title>
 </head>
 
-<body class="fundinhoA">
+<body class="fundinhoB">
 
-            <div>
-            <center><h2 class="texto">Produto</h2></center>
-    <nav>
+        <div>
+
+    <nav> <!--barra superior-->
         <div class="barra-superior">
             <a href="profile.php">
                 <i class="bi bi-person-circle perfil"></i>
@@ -29,52 +29,59 @@
         </div>
     </nav>
 
-    <div class="divLoginD">
-            <div class="divLogin divLogin-left">
-                <h2 class="texto">Produto</h2>
-                <form method="post" action="pagina1Aluno.php" id="formcadastro" name="formcadastro" >
-                <br>
-                <input type="text" name="Placa do caminhão" id="Placa do caminhão" size="20" class="caixaTexto" placeholder="Placa do caminhão"><br />
-                <br>
-                <input type="text" name="Nome do motorista" id="Nome do motorista" size="20" class="caixaTexto" placeholder="Nome do motorista"><br />
-                <br>
-                <input type="text" name="Container" id="Container" size="20" class="caixaTexto" placeholder="Container"><br />
-                <br>
-                <input type="text" name="Navio" id="Navio" size="20" class="caixaTexto" placeholder="Navio"><br/>
-                <br>
-                <input type="text" name="Cliente" id="Cliente" size="20" class="caixaTexto" placeholder="Cliente"><br/>
-                </form>
-                <br>
-                <center><input type="submit" value="registrar" class="botao"/></center>
-                <p></p>
-        <div>
+    <div class="divLoginE"> <!-- tela 1 fundo-->
+     <div class="divlogin divLogin-mit"> <!-- tela 2 fundo-->
 
-            <center><input type="submit" value="registrar" class="botao"/></center>
-                <p></p>
-            </div>
-            </div>
+     <div class="divlogin divLogin-tig">
+       <h1 class="texto">Produtos</h1>
+     </div>
+
+       <div class="divlogin divLogin-rig">
+            <input type="text" name="  Produto_faltando" size="30" class="caixaTexto" placeholder="Produto faltando"><br />
+       </div>
+                <table border ="1" class="divlogin divLogin-mith">  <!-- primira tabela-->
+                        <tr>
+                            <td><input type="text" name="UN" size="20" class="caixaText3" placeholder=" UN"><br/></td>
+                            <td><input type="text" name="QTD" size="20" class="caixaText3" placeholder=" QTD"><br/></td>
+                        </tr> 
+                        <tr>
+                            <td><input type="text" name="R$/UNIT" size="20" class="caixaText3" placeholder=" R$/UNIT"><br/></td>
+                            <td><input type="text" name="R$ TOTAL" size="20" class="caixaText3" placeholder=" R$ TOTAL"><br/></td>  
+                        </tr>   
+                </table>
+    
+       <div class="divlogin divLogin-big">
+            <input type="text" name="  Produto_faltando" size="30" class="caixaTexto" placeholder="Produto faltando"><br />
+       </div>
+
+        <div class="divlogin divLogin-vira">
+                <input type="text" name="Faltando" size="10" class="caixaTexto" placeholder="Faltando?"><br />
         </div>
 
-            <center><input type="submit" value="registrar" class="botao"/></center>
-                <p></p>
-            </div>
-            <div class="divLogin divLogin-Right">
-                <input type="text" name="Tipo" id="Tipo" size="20" class="caixaTexto" placeholder="Tipo"><br/>
-                <br>
-                <input type="text" name="Lacre" id="Lacre" size="20" class="caixaTexto" placeholder="Lacre"><br/>
-                <br>
-                <input type="text" name="LacreSIF" id="LacreSIF" size="20" class="caixaTexto" placeholder="Lacre SIF"><br/>
-                <br>
-                <input type="text" name="Temperatura" id="Temperatura" size="20" class="caixaTexto" placeholder="Temperatura"><br/>
-                <br>
-                <input type="text" name="IMO" id="IMO" size="20" class="caixaTexto" placeholder="IMO"><br/>
-                <br>
-                <input type="text" name="N°ONU" id="N°ONU" size="20" class="caixaTexto" placeholder="N°ONU"><br/>
-                <center><input type="submit" value="registrar" class="botao"/></center>
-                <p></p>
-            </div>
+        <div class="divlogin divLogin-vita">
+                <input type="text" name="Avariado" size="10" class="caixaTexto" placeholder="Avariado?"><br />
         </div>
 
+        <div class="divlogin divLogin-tira">
+                <input type="text" name="Avariado" size="10" class="caixaTexto" placeholder="Avariado?"><br />
+        </div>
+
+        <div class="divlogin divLogin-vina">
+                <input type="text" name="Faltando" size="10" class="caixaTexto" placeholder="Faltando?"><br />
+        </div>
+
+        <div class="divlogin divLogin-rigth"> <!-- segunda tabela-->
+            <table border ="1"> 
+                            <tr>
+                                <td><input type="text" name="UN" size="16" class="caixaText" placeholder=" UN"><br/></td>
+                                <td><input type="text" name="QTD" size="16" class="caixaText" placeholder=" QTD"><br/></td>
+                                <td><input type="text" name="R$/UNIT" size="16" class="caixaText" placeholder=" R$/UNIT"><br/></td>
+                                <td><input type="text" name="R$ TOTAL" size="16" class="caixaText" placeholder=" R$ TOTAL"><br/></td>
+                            </tr>     
+                    </table>
+                </div>
+            </div>
+    </div>
         <nav class="menu-lateral">
 
 <div class="btn-expandir">
