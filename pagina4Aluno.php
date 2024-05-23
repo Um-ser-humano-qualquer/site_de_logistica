30c0631e913e50acb933e7ede96dc73232cdd4ed
Ruthy Hellen Dias <142604700+R2618@users.noreply.github.com>
2024-05-23 07:20:06 -0300

    alterações nas páginas 2,3 e 4. Mudanças nos css

--- pagina4Aluno.php
+++ pagina4Aluno.php
@@ -1,22 +1,12 @@
 <html lang="en"><head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="styleMenu.css">
     <link rel="stylesheet" href="styleDoBotaoGeral.css">
     <link rel="stylesheet" href="style.css">
-<<<<<<< HEAD
     <link rel="stylesheet" href="stylepgA.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-=======
-    <link rel="stylesheet" href="stylepgC.css">
-    <link rel="stylesheet" href="stylepgV.css">
     <meta name="viewport" content="w1.0">
->>>>>>> 5e297a1007160861cbcaa8c39d2cd8d346588597
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     
-    <title>Conteiner</title>
-
-
-<<<<<<< HEAD
     <title>Carga</title>
 </head>
 
@@ -29,30 +19,7 @@
             <a href="profile.php">
                 <i class="bi bi-person-circle perfil"></i>
             </a>
-=======
 </head>
-
-<body class="fundinho">
-    
-    <div class="divMain">
-        <div class="divLogin divLogin-left">
-            <h2 class="texto">Vistoria e Conferência - Carga </h2>    
-            <form method="POST0" action="processoAlunopg2.php">
-                <br>
-                <input type="text" name="nota" size="20" class="caixaTexto" placeholder="Nota fiscal"><br>
-                <br>
-                <input type="text" name="pedido" size="20" class="caixaTexto" placeholder="Pedido de compra">
-                <br>
-                <br>
-                <input type="text" name="doca" size="20" class="caixaTexto" placeholder="Doca"><br>
-                <br>
-                <input type="submit" class="botao">
-            </form>
->>>>>>> 5e297a1007160861cbcaa8c39d2cd8d346588597
-        </div>
-    </div>
-
-<<<<<<< HEAD
     <div class="divLoginE"> <!-- tela 1 fundo-->
      <div class="divlogin divLogin-mit"> <!-- tela 2 fundo-->
 
@@ -108,72 +75,12 @@
     </div>
         <nav class="menu-lateral">
 
-<div class="btn-expandir">
-    <i class="bi bi-list" id="btn-exp"></i>
-</div>
-
-<ul> <!-- menu -->
-    <li class="item-menu">
-        <a href="pagina1Aluno.php">
-            <span class="icon"><i class="bi bi-house"></i></span>
-            <span class="txt-link">Home</span>
-        </a>
-    </li>
-    <li class="item-menu">
-        <a href="pagina2Aluno.php">
-            <span class="icon"><i class="bi bi-columns-gap"></i></span>
-            <span class="txt-link">Recebimento</span>
-        </a>
-    </li>
-    <li class="item-menu">
-        <a href="pagina3aluno.php">
-            <span class="icon"><i class="bi bi-signpost-2-fill"></i></span>
-            <span class="txt-link">Movimentação</span>
-        </a>
-    </li>
-    <li class="item-menu ativo">
-        <a href="pagina4Aluno.php">
-            <span class="icon"><i class="bi bi-box-seam-fill"></i></span>
-            <span class="txt-link">Estoque</span>
-        </a>
-    </li>
-    <li class="item-menu">
-        <a href="">
-            <span class="icon"><i class="bi bi-stack"></i></span>
-            <span class="txt-link">Picking</span>
-        </a>
-    </li>
-    <li class="item-menu">
-        <a href="">
-            <span class="icon"><i class="bi bi-clipboard-data-fill"></i></span>
-            <span class="txt-link">Expedição</span>
-        </a>
-    </li>
-    <li class="item-menu">
-        <a href="">
-            <span class="icon"><i class="bi bi-file-earmark-text-fill"></i></span>
-            <span class="txt-link">Relatórios</span>
-        </a>
-    </li>
-    <li class="item-menu">
-        <a href="">
-            <span class="icon"><i class="bi bi-pin-map-fill"></i></span>
-            <span class="txt-link">Controle</span>
-        </a>
-    </li>
-</ul>
-
-</nav>
-
 <script src="js/menu.js"></script> 
 
 </center>
 
-</body>
-=======
     <?php
         include 'menu.php'
     ?>
     </body>
->>>>>>> 5e297a1007160861cbcaa8c39d2cd8d346588597
 </html>
