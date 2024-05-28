738402b5e40c7735fc960d5b4e35e01a1439333f
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-05-28 08:23:39 -0300

    ajeitando o posicionamento das linhas de código da página 3 do aluno

--- pagina3Aluno.php
+++ pagina3Aluno.php
@@ -2,16 +2,6 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-<meta charset="UTF-8"> <!-- style's css-->
-
-    <link rel="stylesheet" href="styleDoMenu.css">
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="Menu.css">
-    <link rel="stylesheet" href="styleMenu.css">
-    <link rel="stylesheet" href="styleDoBotaoGeral.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="stylepgC.css">
-    <link rel="stylesheet" href="css/styleMenu.css">
     <link rel="stylesheet" href="css/styleDoBotaoGeral.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/stylepgC.css">
@@ -22,61 +12,58 @@
 </head>
 
 <body class="fundinhoA">
-    <nav>
-        <div class="barra-superior">
-            <a href="profile.php">
-                <i class="bi bi-person-circle perfil"></i>
-            </a>
-        </div>
-    </nav>
 
     <div class="divLoginD">
-            <div class="divLogin divLogin-left">
-                <h2 class="texto">Produto</h2>
-    <div class="divLoginD">
-            <div class="divLogin divLogin-left">
-                <h2 class="texto">Produto</h2>
-                <form method="post" action="pagina1Aluno.php" id="formcadastro" name="formcadastro" >
-                <br>
-                <input type="text" name="Placa do caminhão" id="Placa do caminhão" size="20" class="caixaTexto" placeholder="Placa do caminhão"><br />
-                <br>
-                <input type="text" name="Nome do motorista" id="Nome do motorista" size="20" class="caixaTexto" placeholder="Nome do motorista"><br />
-                <br>
-                <input type="text" name="Container" id="Container" size="20" class="caixaTexto" placeholder="Container"><br />
-                <br>
-                <input type="text" name="Navio" id="Navio" size="20" class="caixaTexto" placeholder="Navio"><br/>
-                <br>
-                <input type="text" name="Cliente" id="Cliente" size="20" class="caixaTexto" placeholder="Cliente"><br/>
-                </form>
-                <br>
-                <center><input type="submit" value="registrar" class="botao"/></center>
-                <p></p>
-        <div>
-
-            <center><input type="submit" value="registrar" class="botao"/></center>
-                <p></p>
+        <div class="divLogin divLogin-left">
+            <h2 class="texto">Produto</h2>
+            <div class="divLoginD">
+                <div class="divLogin divLogin-left">
+                    <h2 class="texto">Produto</h2>
+                    <form method="post" action="pagina1Aluno.php" id="formcadastro" name="formcadastro" >
+                        <br>
+                        <input type="text" name="Placa do caminhão" id="Placa do caminhão" size="20" class="caixaTexto" placeholder="Placa do caminhão"><br />
+                        <br>
+                        <input type="text" name="Nome do motorista" id="Nome do motorista" size="20" class="caixaTexto" placeholder="Nome do motorista"><br />
+                        <br>
+                        <input type="text" name="Container" id="Container" size="20" class="caixaTexto" placeholder="Container"><br />
+                        <br>
+                        <input type="text" name="Navio" id="Navio" size="20" class="caixaTexto" placeholder="Navio"><br/>
+                        <br>
+                        <input type="text" name="Cliente" id="Cliente" size="20" class="caixaTexto" placeholder="Cliente"><br/>
+                    </form>
+                    <br>
+                    <center>
+                    <input type="submit" value="registrar" class="botao"/></center>
+                    <p></p>
+                    <div>
+                        <center>
+                        <input type="submit" value="registrar" class="botao"/></center>
+                        <p></p>
+                    </div>
+                </div>
             </div>
-            </div>
+            <center>
+            <input type="submit" value="registrar" class="botao"/></center>
+            <p></p>
         </div>
-            <center><input type="submit" value="registrar" class="botao"/></center>
-                <p></p>
-            </div>
-            <div class="divLogin divLogin-Right">
-                <input type="text" name="Tipo" id="Tipo" size="20" class="caixaTexto" placeholder="Tipo"><br/>
-                <br>
-                <input type="text" name="Lacre" id="Lacre" size="20" class="caixaTexto" placeholder="Lacre"><br/>
-                <br>
-                <input type="text" name="LacreSIF" id="LacreSIF" size="20" class="caixaTexto" placeholder="Lacre SIF"><br/>
-                <br>
-                <input type="text" name="Temperatura" id="Temperatura" size="20" class="caixaTexto" placeholder="Temperatura"><br/>
-                <br>
-                <input type="text" name="IMO" id="IMO" size="20" class="caixaTexto" placeholder="IMO"><br/>
-                <br>
-                <input type="text" name="N°ONU" id="N°ONU" size="20" class="caixaTexto" placeholder="N°ONU"><br/>
-                <center><input type="submit" value="registrar" class="botao"/></center>
-                <p></p>
-            </div>
+        <div class="divLogin divLogin-Right">
+            <input type="text" name="Tipo" id="Tipo" size="20" class="caixaTexto" placeholder="Tipo"><br/>
+            <br>
+            <input type="text" name="Lacre" id="Lacre" size="20" class="caixaTexto" placeholder="Lacre"><br/>
+            <br>
+            <input type="text" name="LacreSIF" id="LacreSIF" size="20" class="caixaTexto" placeholder="Lacre SIF"><br/>
+            <br>
+            <input type="text" name="Temperatura" id="Temperatura" size="20" class="caixaTexto" placeholder="Temperatura"><br/>
+            <br>
+            <input type="text" name="IMO" id="IMO" size="20" class="caixaTexto" placeholder="IMO"><br/>
+            <br>
+            <input type="text" name="N°ONU" id="N°ONU" size="20" class="caixaTexto" placeholder="N°ONU"><br/>
+            <center>
+                <input type="submit" value="registrar" class="botao"/>
+            </center>
+            <p></p>
         </div>
+    </div>
         
     <?php
         include 'menu.php'
