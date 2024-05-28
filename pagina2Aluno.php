55a9562888d0574b1503b789dca7231a7283917a
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-05-28 08:13:46 -0300

    frescura

--- pagina2Aluno.php
+++ pagina2Aluno.php
@@ -27,10 +27,6 @@
     <div class="divLoginG">
         <div class="divLogin divLogin-left">
             <h2 class="texto">Vistoria e Conferência - Conteiner </h2>
-<<<<<<< HEAD
-            <a href="pagina2.1.php">Carga</a>
-=======
->>>>>>> 02f4d931a830c687c1ebd1cff870583f839d8dc1
             <form method="POST0" action="processoAlunopg2.php" >
             <br>
             <input type="text" name="placa_caminhao" size="20" class="caixaTexto" placeholder="Placa do caminhão"><br />
