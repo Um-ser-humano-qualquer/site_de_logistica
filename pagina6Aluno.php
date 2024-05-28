5ef5983691216c6e458faf99661251c3deee7b6f
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-05-28 12:29:33 -0300

    modificação na página 6 do aluno

--- pagina6Aluno.php
+++ pagina6Aluno.php
@@ -1,38 +1,68 @@
-<html lang="en"><head>
-<meta charset="UTF-8">
-    <link rel="stylesheet" href="css/Menu.css">
-    <link rel="stylesheet" href="css/styleDoBotaoGeral.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/stylepgC.css">
-    <link rel="stylesheet" href="css/stylepgV.css">
-    <meta name="viewport" content="w1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    
-    <title>Document</title>
-
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/styleDoBotaoGeral.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/stylepgC.css">
+        <link rel="stylesheet" href="css/stylepgV.css">
+        <meta name="viewport" content="w1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <title>Document</title>
     </head>
-<body class="fundinho">
+    
+    <body class="fundinho">
+        <div class="divMain2">
+            <div class="divLogin divLogin-pedido">
+                <h2 class="texto">Produtos</h2>           
+                <form method="POST0" action="processoCarga.php">
+                    <br>
+                    <input type="text" name="nota" size="28" class="caixaTexto" placeholder="Produto faltando">
+                    <br>
+                    <br>
+                    <table>
+                        <tr>
+                            <th>&nbsp;</th>
+                            <th>QTD</th>
+                            <th>R$/UNIT</th>
+                            <th>R$ TOTAL</th>
+                        </tr>
+                        <tr>
+                            <td>UN</td>
+                            <td><input type="text"></td>
+                            <td><input type="text"></td>
+                            <td><input type="text"></td>
+                        </tr>
+                    </table>
+                    <br>
+                    <div class="sla">
+                        <div class="faltando">
+                            <form action="post">
+                                <h2>Faltando?</h2>
+                                <div class="posicao">
+                                    <input type="checkbox">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="avariado">
+                            <form action="post">
+                                <h2>Avariado?</h2>
+                                <div class="posicao">
+                                    <input type="checkbox">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <br>
+                    <input type="submit" class="botao">
+                </form>
+            </div>        
+        </div>
 
-    <div class="divMain2">
-        <div class="divLogin divLogin-pedido">
-            <h2 class="texto">Produtos</h2>           
-            <form method="POST0" action="processoCarga.php">
-                <br>
-                <input type="text" name="nota" size="28" class="caixaTexto" placeholder="Produto faltando"><br>
-                <br>
-                <input type="text" name="pedido" size="10" class="caixaTexto" placeholder="Faltando?">
-                <input type="text" name="doca" size="10" class="caixaTexto" placeholder="Avariado?"><br>
-                <br>
-                <input type="submit" class="botao">
-            </form>
-        </div>        
-    </div>
+        <script src="js/popup.js"></script>
 
-<script src="js/popup.js"></script>
+        <?php
+            include 'menu.php'
+        ?>
 
-<?php
-    include 'menu.php'
-?>
-
-</body>
+    </body>
 </html>
