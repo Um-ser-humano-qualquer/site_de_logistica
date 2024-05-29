bfc71ab3cbf3afcc9d25336579b260dcd2af8989
Petra Gianni Sanhueza Maffezzoli <142603915+P-Maffezzoli@users.noreply.github.com>
2024-05-29 12:23:43 -0300

    continuação pg5

--- menu.php
+++ menu.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styleMenu.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <title>Menu geral</title>
 </head>
 <body>
     
--- pagina5Aluno.php
+++ pagina5Aluno.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html lang="en"><head>
+<html lang="en">
 
-<meta charset="UTF-8">
+<head>
 
-    
+    <meta charset="UTF-8">
+
+
     <link rel="stylesheet" href="css/styleDoBotaoGeral.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/stylepgE.css">
@@ -15,76 +17,90 @@
 
 <body class="fundo">
 
-      <div>
-       
-    <div class="divMain">
-         <form method="post" action="pagina1Aluno.php" id="formcadastro" name="formcadastro">
-            <div class="divLogin-left">
-                <h2 class="texto">Produtos nas docas</h2>       
-                <br>
-                <input type="text" name="Pedido1" id="Pedido 1" size="15" class="caixaTexto" placeholder="Pedido 1"><br>
-                <input type="text" name="Pedido2" id="Pedido 2" size="15" class="caixaTexto" placeholder="Pedido 2"><br>
-                <input type="text" name="Pedido3" id="Pedido 3" size="15" class="caixaTexto" placeholder="Pedido 3"><br>
-                <input type="text" name="Pedido4" id="Pedido 4" size="15" class="caixaTexto" placeholder="Pedido 4"><br>
-                <input type="text" name="Pedido5" id="Pedido 5" size="15" class="caixaTexto" placeholder="Pedido 5"><br>
-                <input type="text" name="Pedido6" id="Pedido 6" size="15" class="caixaTexto" placeholder="Pedido 6"><br>
-                <input type="text" name="Pedido7" id="Pedido 7" size="15" class="caixaTexto" placeholder="Pedido 7"><br>
-                <input type="text" name="Pedido8" id="Pedido 8" size="15" class="caixaTexto" placeholder="Pedido 8"><br>
-                <input type="text" name="Pedido9" id="Pedido 9" size="15" class="caixaTexto" placeholder="Pedido 9"><br>
-                <input type="text" name="Pedido10" id="Pedido 10" size="15" class="caixaTexto" placeholder="Pedido 10"><br>
-            
-            
-            
-            <div class="divLogin-mid"> 
-                <h2>Docas</h2>
-                <br>
-                <br>
-                <span class="Disp">disponivel</span>
-                <br>
-                <br>
-                <span class="Disp">disponivel</span>
-                <br>
-                <br>
-                <span class="Disp">disponivel</span>
-                <br>
-                <br>
-                <span class="Disp">disponivel</span>
-                <br>
-                <br>
-                <span class="Disp">disponivel</span>
-                <br>
-                <br>
-                <span class="Disp">disponivel</span>
-                <br>
-                <br>
-                <span class="Disp">disponivel</span>
-                <br>
-                <br>
-                <span class="Disp">disponivel</span>
-                <br>
-                <br>
-                <span class="Disp">disponivel</span>
-                <br>
-                <br>
-                <span class="Disp">disponivel</span>
-                <br>
-                <br>
+    <div>
 
-            </div>
+        <div class="divMain">
 
+            <form method="post" action="pagina7Aluno.php" id="confirmarDocas" name="confirmarDocas">
+                <div class="divLogin-left">
+                    <h2 class="texto">Produtos nas docas</h2>
+                    <br>
+                    <input type="text" name="Pedido1" id="Pedido 1" size="15" class="caixaTexto" placeholder="Pedido 1">
+                    <br>
+                    <br>
+                    <input type="text" name="Pedido1" id="Pedido 1" size="15" class="caixaTexto" placeholder="Pedido 2">
+                    <br>
+                    <br>
+                    <input type="text" name="Pedido1" id="Pedido 1" size="15" class="caixaTexto" placeholder="Pedido 3">
+                    <br>
+                    <br>
+                    <input type="text" name="Pedido1" id="Pedido 1" size="15" class="caixaTexto" placeholder="Pedido 4">
+                    <br>
+                    <br>
+                    <input type="text" name="Pedido1" id="Pedido 1" size="15" class="caixaTexto" placeholder="Pedido 5">
+                    <br>
+                    <br>
+                    <input type="text" name="Pedido1" id="Pedido 1" size="15" class="caixaTexto" placeholder="Pedido 6">
+                    <br>
+                    <br>
+                    <input type="text" name="Pedido1" id="Pedido 1" size="15" class="caixaTexto" placeholder="Pedido 7">
+                    <br>
+                    <br>
+                    <input type="text" name="Pedido1" id="Pedido 1" size="15" class="caixaTexto" placeholder="Pedido 8">
+                    <br>
+                    <br>
+                    <input type="text" name="Pedido1" id="Pedido 1" size="15" class="caixaTexto" placeholder="Pedido 9">
+                    <br>
+                    <br>
+                    <input type="text" name="Pedido1" id="Pedido 1" size="15" class="caixaTexto" placeholder="Pedido 10">
+                    <br>
+                    <br>
+                </div>
+
+                <div class="divLogin-mid">
+                    <h2>Docas</h2>
+                    <br>
+                    <input class="caixaTexto" placeholder="Disponível" size="15"></input> <input type="submit" class="botao" value="OK">
+                    <br>
+                    <br>
+                    <input class="caixaTexto" placeholder="Disponível" size="15"></input> <input type="submit" class="botao" value="OK">
+                    <br>
+                    <br>
+                    <input class="caixaTexto" placeholder="Disponível" size="15"></input> <input type="submit" class="botao" value="OK">
+                    <br>
+                    <br>
+                    <input class="caixaTexto" placeholder="Disponível" size="15"></input> <input type="submit" class="botao" value="OK">
+                    <br>
+                    <br>
+                    <input class="caixaTexto" placeholder="Disponível" size="15"></input> <input type="submit" class="botao" value="OK">
+                    <br>
+                    <br>
+                    <input class="caixaTexto" placeholder="Disponível" size="15"></input> <input type="submit" class="botao" value="OK">
+                    <br>
+                    <br>
+                    <input class="caixaTexto" placeholder="Disponível" size="15"></input> <input type="submit" class="botao" value="OK">
+                    <br>
+                    <br>
+                    <input class="caixaTexto" placeholder="Disponível" size="15"></input> <input type="submit" class="botao" value="OK">
+                    <br>
+                    <br>
+                    <input class="caixaTexto" placeholder="Disponível" size="15"></input> <input type="submit" class="botao" value="OK">
+                    <br>
+                    <br>
+                    <input class="caixaTexto" placeholder="Disponível" size="15"></input> <input type="submit" class="botao" value="OK">
+
+
+                </div>   
             </form>
-                <br>
-        
-            </div>
         </div>
-           
-            
-        
-    <?php
-        include 'menu.php'
-    ?>
 
-</center>
+    </div>
+    </div>
+
+    <?php
+    include 'menu.php'
+        ?>
 
 </body>
+
 </html>
