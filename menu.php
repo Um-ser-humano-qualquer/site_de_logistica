6b96a3a38e8b966f60e08e5bdd9e5b0bf7d09cdd
Ruthy Hellen Dias <142604700+R2618@users.noreply.github.com>
2024-06-02 14:45:03 -0300

    criação da pagina7Aluno.php, modificações no styles V e C, alteração no menu e adição de uma nova imagem.

--- menu.php
+++ menu.php
@@ -55,7 +55,7 @@
                 </a>
             </li>
             <li class="item-menu post">
-                <a href="">
+                <a href="pagina7Aluno.php">
                     <span class="icon"><i class="bi bi-box-seam-fill"></i></span>
                     <span class="txt-link">Estoque</span>
                 </a>
---
+++ pagina7Aluno.php
@@ -0,0 +1,147 @@
+<html lang="pt-br">
+    <head>
+        <link rel="stylesheet" href="css/styleDoBotaoGeral.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/StylepgC.css">
+        <link rel="stylesheet" href="css/stylepgV.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <title>Movimentação2</title>
+    </head>
+    
+    <body class="fundo">
+        <div class="divMain2">
+            <div class="divMain3"></div>
+            <div class="divLogin divLogin-pedido posicao-pedidoL">
+                <h1 class="texto">Produto</h1>           
+                <form method="POST0" action="processoCarga.php">
+                    <br>
+                    <input type="text" name="nota" size="28" class="caixaTexto" placeholder="Produto faltando">
+                    <br>
+                    <br>
+                    <table class="texto">
+                        <tr>
+                            <th></th>
+                            <th class="QTD">
+                                <h3>QTD</h3>
+                            </th>
+                            <th class="UNIT">
+                                <h3>R$/UNIT</h3>
+                            </th>
+                            <th class="TOTAL">
+                                <h3>R$ TOTAL</h3>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <h3 class="UN">UN</h3>
+                            </th>
+                            <div class="Btable"></div>
+                            <div class="posicaoTabela">
+                                <td>    
+                                    <input type="text" class="tableL texto">
+                                </td>
+                                <td>
+                                    <input type="text" class="tableC texto">
+                                </td>
+                                <td>
+                                    <input type="text" class="tableR texto">
+                                </td>
+                            </div> 
+                        </tr>
+                    </table>
+                    <br>
+                    <div class="sla">
+                        <div class="faltando">
+                            <form action="post">
+                                <h2>Faltando?</h2>
+                                <div class="posicao">
+                                    <input type="checkbox">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="avariado">
+                            <form action="post">
+                                <h2>Avariado?</h2>
+                                <div class="posicao">
+                                    <input type="checkbox">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <br>
+                    <input type="submit" class="botao">
+                </form>
+            </div>     
+            <div class="divLogin divLogin-pedido posicao-pedidoR">
+                <h1 class="texto">Produto</h1>           
+                <form method="POST0" action="processoCarga.php">
+                    <br>
+                    <input type="text" name="nota" size="28" class="caixaTexto" placeholder="Produto faltando">
+                    <br>
+                    <br>
+                    <table class="texto">
+                        <tr>
+                            <th></th>
+                            <th class="QTD">
+                                <h3>QTD</h3>
+                            </th>
+                            <th class="UNIT">
+                                <h3>R$/UNIT</h3>
+                            </th>
+                            <th class="TOTAL">
+                                <h3>R$ TOTAL</h3>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <h3 class="UN">UN</h3>
+                            </th>
+                            <div class="Btable"></div>
+                            <div class="posicaoTabela">
+                                <td>    
+                                    <input type="text" class="tableL texto">
+                                </td>
+                                <td>
+                                    <input type="text" class="tableC texto">
+                                </td>
+                                <td>
+                                    <input type="text" class="tableR texto">
+                                </td>
+                            </div> 
+                        </tr>
+                    </table>
+                    <br>
+                    <div class="sla">
+                        <div class="faltando">
+                            <form action="post">
+                                <h2>Faltando?</h2>
+                                <div class="posicao">
+                                    <input type="checkbox">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="avariado">
+                            <form action="post">
+                                <h2>Avariado?</h2>
+                                <div class="posicao">
+                                    <input type="checkbox">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <br>
+                    <input type="submit" class="botao">
+                </form>
+            </div>   
+
+     <div class="linhaB"></div> <!-- Importante não deletar (barra lateral)-->
+        </div>
+
+        <script src="js/popup.js"></script> <!-- Faço a menor ideia (pergumtar mais tarde)-->
+
+        <?php 
+            include 'menu.php'
+        ?>
+
+    </body>
+</html>
