d3d4269e2d78a3f797b38494a80c3e1c089dce09
Petra Gianni Sanhueza Maffezzoli <142603915+P-Maffezzoli@users.noreply.github.com>
2024-06-04 11:31:45 -0300

    início do novo BD(login de aluno)

--- processoLoginAluno.php
+++ processoLoginAluno.php
@@ -1,42 +1,40 @@
 <?php
-    session_start();
+session_start();
 
-    $hostname = "127.0.0.1";
-    $user = "root";
-    $password = "";
-    $database = "sa_logistica";
+$hostname = "127.0.0.1";
+$user = "root";
+$password = "";
+$database = "logistica";
 
-    $connect = new mysqli($hostname, $user, $password, $database);
+$connect = new mysqli($hostname, $user, $password, $database);
 
-    if ($connect->connect_errno) {
-        echo "Falha ao conectar ao MySQL: " . $connect->connect_error;
+if ($connect->connect_errno) {
+    echo "Falha ao conectar ao MySQL: " . $connect->connect_error;
+    exit();
+} else
+    if (isset($_POST['username']) && isset($_POST['password']))
+        $username = $connect->real_escape_string($_POST['username']);
+$password = $connect->real_escape_string($_POST['password']);
+
+$SQL = "SELECT `id`, `username` 
+            FROM `aluno` 
+            WHERE `username` = ? AND `password` = ?";
+
+if ($stmt = $connect->prepare($SQL)) {
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+
+    if ($resultado->num_rows != 0) {
+        $row = $resultado->fetch_assoc();
+        $_SESSION['id'] = $row['id'];
+        $_SESSION['username'] = $row['username'];
+        $connect->close();
+
+        header('Location: pagina1Aluno.php', true, 301);
         exit();
-    } else 
-        if(isset($_POST['emailAluno']) && isset($_POST['senhaAluno'])) 
-            $emailAluno = $connect->real_escape_string($_POST['emailAluno']);
-            $senhaAluno = $connect->real_escape_string($_POST['senhaAluno']);
-
-            $SQL = "SELECT `idAluno`, `emailAluno` 
-            FROM `alunos` 
-            WHERE `emailAluno` = ? AND `senhaAluno` = ? AND `ativo` = 's';";
-
-            if($stmt = $connect->prepare($SQL)) {
-                $stmt->bind_param("ss", $emailAluno, $senhaAluno);
-                $stmt->execute();
-                $resultado = $stmt->get_result();
-
-                if($resultado->num_rows != 0) {
-                    $row = $resultado->fetch_assoc();
-                    $_SESSION['idAluno'] = $row['idAluno'];
-                    $_SESSION['nomeAluno'] = $row['emailAluno'];
-                    $connect->close();
-
-                    header('Location: pagina1Aluno.php', true, 301);
-                    exit();
-                } else {
-                    $connect->close();
-                    header('Location: index.php', true, 301);
-                }
-            } 
-        
-?>
+    } else {
+        $connect->close();
+        header('Location:index.php?error=Incorect User name or password', true, 301);
+    }
+}
