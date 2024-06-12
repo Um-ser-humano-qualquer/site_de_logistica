6d9b6e0edb3acb8e38c159957f878eb314583204
T. Veran Bráz <143181028+ThantonVeran@users.noreply.github.com>
2024-06-12 12:35:21 -0300

    picking
    denis

---
+++ pagina9Aluno.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/styleDoBotaoGeral.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/stylepg9.css">
+        <meta name="viewport" content="w1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <link rel="shortcut icon" href="img/icone_creati.ico" type="image/x-icon">
+    <title>Picking</title>
+    
+</head>
+<body>
+    <div class="divMain">
+        <div class="benes">
+
+        </div>
+        <div class="">
+
+        </div>
+    </div>
+    <?php
+        include 'MenuAluno.php'
+    ?>
+</body>
+</html>
