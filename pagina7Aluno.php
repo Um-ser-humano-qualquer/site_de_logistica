1e602247727ee6f6098a71f06479653ef9111b66
Petra Gianni Sanhueza Maffezzoli <142603915+P-Maffezzoli@users.noreply.github.com>
2024-06-18 12:38:41 -0300

    continuando pagina 7
    to ficando louca

--- pagina7Aluno.php
+++ pagina7Aluno.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movimentação2</title>
     <link rel="stylesheet" href="css/styleDoBotaoGeral.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.cs">
     <link rel="stylesheet" href="css/stylepgG.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="shortcut icon" href="img/icone_creati.ico" type="image/x-icon">
@@ -13,40 +13,41 @@
 </head>
 
 <body class="fundo">
-    <div class="divMain ">
-        <div class="divLogin-mid">
+    <div class="divMain2 ">
+        <div class="div-mid">
             <div class="texto">
                 <input type="text" size="15">
                 <br><br>
                 <input type="text" size="15">
             </div>
-            <div class="tabela">
-                <table>
-                    <tr class="">
 
-                        <th class="">
-                            <span>QNT</span>
-                        </th>
-                        <th class="">
-                            <span>R$/UNIT</span>
-                        </th>
-                        <th class="">
-                            <span>POSIÇÃO</span>
-                        </th>
-                    </tr>
+            <div class="tabela">
+                <tr>
+                    <div class="Btable"></div>
+                    <div class="posicaoTabela">
+                        <td>
+                            <input type="text" class="tableL texto" placeholder="QNT">
+                        </td>
+                        <td>
+                            <input type="text" class="tableC texto" placeholder="R$/UNIT">
+                        </td>
+                        <td>
+                            <input type="text" class="tableR texto" placeholder="POSIÇÃO">
+                        </td>
+                    </div>
+                </tr>
                 </table>
             </div>
-            <div class="divLogin-ok">
-
-            </div>
         </div>
+    
+        <div class="divLogin-ok">
+        
     </div>
-
-    <script src="js/popup.js"></script>
-
+        
+    </div>
     <?php
     include 'menuAluno.php'
-        ?>
+    ?>
 
 </body>
 
