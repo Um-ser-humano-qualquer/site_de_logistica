cf237c6482e99569c312cd6a962df75eaea8581e
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-06-18 12:36:57 -0300

    mais uma tela feita

--- pagina14Aluno.php
+++ pagina14Aluno.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="css/styleDoBotaoGeral.css">
@@ -12,73 +12,73 @@
     </head>
 
     <body class="fundo">
-        <div>
-            <div class="divMain inline">
-            <form method="post" action="pagina7Aluno.php" id="confirmarDocas" name="confirmarDocas">
-                <div class="divLogin-left">
-                    <h2 class="texto">Produtos nas docas</h2>
-                    <br>
-                    <input type="text" name="Pedido1" id="Pedido1" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº1">
-                    <br>
-                    <input type="text" name="Pedido1" id="Pedido2" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº2">
-                    <br>
-                    <input type="text" name="Pedido1" id="Pedido3" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº3">
-                    <br>
-                    <input type="text" name="Pedido1" id="Pedido4" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº4">
-                    <br>
-                    <input type="text" name="Pedido1" id="Pedido5" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº5">
-                    <br>
-                    <input type="text" name="Pedido1" id="Pedido6" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº6">
-                    <br>
-                    <input type="text" name="Pedido1" id="Pedido7" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº7">
-                    <br>
-                    <input type="text" name="Pedido1" id="Pedido8" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº8">
-                    <br>
-                    <input type="text" name="Pedido1" id="Pedido9" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº9">
-                    <br>
-                    <input type="text" name="Pedido1" id="Pedido10" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº10">
-                    <br>
-                </div>
-                <div class="divLogin-mid">
-                    <h2>Docas</h2>
-                    <div class="divLogin-mid posicao-div posicao-div-top"></div>
-                    <div class="divLogin-mid posicao-div"></div>
-                    <div class="divLogin-mid posicao-div"></div>
-                    <div class="divLogin-mid posicao-div"></div>
-                    <div class="divLogin-mid posicao-div"></div>
-                    <div class="divLogin-mid posicao-div"></div>
-                    <div class="divLogin-mid posicao-div"></div>
-                    <div class="divLogin-mid posicao-div"></div>
-                    <div class="divLogin-mid posicao-div"></div>
-                    <div class="divLogin-mid posicao-div"></div>
-                </div>   
-                <div class="OK">
-                    <input type="submit" class="botao btn-OK" value="OK"></input>
-                    <br>
-                    <input type="submit" class="botao btn-OK" value="OK"></input>
-                    <br>
-                    <input type="submit" class="botao btn-OK" value="OK"></input>
-                    <br>
-                    <input type="submit" class="botao btn-OK" value="OK"></input>
-                    <br>
-                    <input type="submit" class="botao btn-OK" value="OK"></input>
-                    <br>
-                    <input type="submit" class="botao btn-OK" value="OK"></input>
-                    <br>
-                    <input type="submit" class="botao btn-OK" value="OK"></input>
-                    <br>
-                    <input type="submit" class="botao btn-OK" value="OK"></input>
-                    <br>
-                    <input type="submit" class="botao btn-OK" value="OK"></input>
-                    <br>
-                    <input type="submit" class="botao btn-OK" value="OK"></input>
-                    <br>
-                </div>
-            </form>
+        <div class="divMain inline">
+        <form method="post" action="pagina7Aluno.php" id="confirmarDocas" name="confirmarDocas">
+            <div class="divLogin-left">
+                <h1 class="texto">Produtos nas docas</h1>
+                <input type="text" name="Pedido1" id="Pedido1" size="15" class="caixaTexto textoPosicao posicaoPedido1" placeholder="Pedido nº1">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido2" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº2">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido3" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº3">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido4" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº4">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido5" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº5">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido6" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº6">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido7" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº7">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido8" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº8">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido9" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº9">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido10" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº10">
+                <br>
+            </div>
+            <div class="divLogin-mid">
+                <h1 class="texto texto-posicao">Docas</h1>
+                <div class="divLogin-mid posicao-div posicao-div-top"></div>
+                <div class="divLogin-mid posicao-div"></div>
+                <div class="divLogin-mid posicao-div"></div>
+                <div class="divLogin-mid posicao-div"></div>
+                <div class="divLogin-mid posicao-div"></div>
+                <div class="divLogin-mid posicao-div"></div>
+                <div class="divLogin-mid posicao-div"></div>
+                <div class="divLogin-mid posicao-div"></div>
+                <div class="divLogin-mid posicao-div"></div>
+                <div class="divLogin-mid posicao-div"></div>
+            </div>   
+            <div class="OK">
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+            </div>
+        </form>
+        <form method="post" action="" id="" name="">
+            <div class="confirmado">
+                <input type="submit" class="btn-confirmado" value="confirmação"></input>
+            </div>
+        </form>
         </div>
-
-    </div>
-    </div>
 
     <?php
     include 'menuAluno.php'
