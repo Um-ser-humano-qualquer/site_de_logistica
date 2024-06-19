a88d0ff640bfea4095e9effab736067b2bb6bb17
Ruthy Hellen Dias <142604700+R2618@users.noreply.github.com>
2024-06-19 12:33:50 -0300

    criação da pag13 e alteração na pag14, imagem nova :D

--- pagina13Aluno.php
+++ pagina13Aluno.php
@@ -1,21 +1,17 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <meta charset="UTF-8">
         <link rel="stylesheet" href="css/styleDoBotaoGeral.css">
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/stylepgE.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         <link rel="shortcut icon" href="img/icone_creati.ico" type="image/x-icon">
-        <title> Doca (Controle)</title>
+        <title> Solicitações </title>
     </head>
 
     <body class="fundo">
         <div class="divMain inline">
-        <form method="post" action="pagina7Aluno.php" id="confirmarDocas" name="confirmarDocas">
             <div class="divLogin-left">
-                <h1 class="texto">Produtos nas docas</h1>
                 <input type="text" name="Pedido1" id="Pedido1" size="15" class="caixaTexto textoPosicao posicaoPedido1" placeholder="Pedido nº1">
                 <br>
                 <input type="text" name="Pedido1" id="Pedido2" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº2">
@@ -37,19 +33,7 @@
                 <input type="text" name="Pedido1" id="Pedido10" size="15" class="caixaTexto textoPosicao" placeholder="Pedido nº10">
                 <br>
             </div>
-            <div class="divLogin-mid">
-                <h1 class="texto texto-posicao">Docas</h1>
-                <div class="divLogin-mid posicao-div posicao-div-top"></div>
-                <div class="divLogin-mid posicao-div"></div>
-                <div class="divLogin-mid posicao-div"></div>
-                <div class="divLogin-mid posicao-div"></div>
-                <div class="divLogin-mid posicao-div"></div>
-                <div class="divLogin-mid posicao-div"></div>
-                <div class="divLogin-mid posicao-div"></div>
-                <div class="divLogin-mid posicao-div"></div>
-                <div class="divLogin-mid posicao-div"></div>
-                <div class="divLogin-mid posicao-div"></div>
-            </div>   
+         
             <div class="OK">
                 <input type="submit" class="btn-OK" value="OK"></input>
                 <br>
@@ -72,12 +56,6 @@
                 <input type="submit" class="btn-OK" value="OK"></input>
                 <br>
             </div>
-        </form>
-        <form method="post" action="" id="" name="">
-            <div class="confirmado">
-                <input type="submit" class="btn-confirmado" value="confirmação"></input>
-            </div>
-        </form>
         </div>
 
     <?php
