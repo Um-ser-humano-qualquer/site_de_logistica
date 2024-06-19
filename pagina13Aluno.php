c0a515fea4b98408e7fc9e7cb700e45d0199326d
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-06-19 08:42:57 -0300

    página de expedição DOIS feita

---
+++ pagina13Aluno.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Expedição</title>
+    </head>
+    <body class="fundoExpedicao">
+        <form action="" method="POST0"></form>
+            <div class="divExpedicao inline">
+                <div class="divExpedicao1">
+                    <input type="text" class="caixaTextoExpedicao"placeholder="Solicitação nº">
+                    <br>
+                    <input type="text" class="caixaTextoExpedicao"placeholder="Solicitação nº">
+                    <br>
+                    <input type="text" class="caixaTextoExpedicao"placeholder="Solicitação nº">
+                    <br>
+                    <input type="text" class="caixaTextoExpedicao"placeholder="Solicitação nº">
+                    <br>
+                    <input type="text" class="caixaTextoExpedicao"placeholder="Solicitação nº">
+                </div>
+                <div class="divExpedicao2">
+                    <input type="text" class="caixaTextoExpedicao"placeholder="Doca">
+                    <br>
+                    <input type="text" class="caixaTextoExpedicao"placeholder="Doca">
+                    <br>
+                    <input type="text" class="caixaTextoExpedicao"placeholder="Doca">
+                    <br>
+                    <input type="text" class="caixaTextoExpedicao"placeholder="Doca">
+                    <br>
+                    <input type="text" class="caixaTextoExpedicao"placeholder="Doca">
+                </div>
+                <div class="divExpedicao3">
+                    <input type="submit" class="botaoExpedicao botaoExpedicaoSuperior" value="Abrir">
+                    <br>
+                    <input type="submit" class="botaoExpedicao" value="Abrir">
+                    <br>
+                    <input type="submit" class="botaoExpedicao" value="Abrir">
+                    <br>
+                    <input type="submit" class="botaoExpedicao" value="Abrir">
+                    <br>
+                    <input type="submit" class="botaoExpedicao" value="Abrir">
+                </div>
+            </div>
+        </form>
+        <?php
+            include "menuAluno.php"
+        ?>
+    </body>
+</html>
