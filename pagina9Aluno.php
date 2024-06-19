5d9a6d7d73eca5261feacb5ba99e5fdea8dde3f1
T. Veran Bráz <143181028+ThantonVeran@users.noreply.github.com>
2024-06-19 08:53:26 -0300

    muchas cosas
    picking em processo

--- pagina9Aluno.php
+++ pagina9Aluno.php
@@ -16,11 +16,49 @@
 
 <body>
     <div class="divMain">
+    <div class="divLogin divLogin-left">
+    <h2 class="texto">Escolha de Picking</h2>         
+                     
+
+            </div>
+    <br>
+    <br>
         <div class="benes">
-     
+       
+    <br>
+        <input type="text" name="solicitação nº" size="10" class="caixaTexto" placeholder="solicitação nº">
+    <br>
+    <br>
+        <input type="text" name="solicitação nº" size="10" class="caixaTexto" placeholder="solicitação nº">
+    <br>
+    <br>
+        <input type="text" name="solicitação nº" size="10" class="caixaTexto" placeholder="solicitação nº">
+    <br>
+    <br>
+        <input type="text" name="solicitação nº" size="10" class="caixaTexto" placeholder="solicitação nº">
+    <br>
+    <br>
+        <input type="text" name="solicitação nº" size="10" class="caixaTexto" placeholder="solicitação nº">
+    <br>
+    <br>
+            
+      
         </div>
         <div class="benis">
-       
+    <input type="submit" size="50" class="botao">
+    <br>
+    <br>
+    <input type="submit" class="botao">
+    <br>
+    <br>
+    <input type="submit" class="botao">
+    <br>
+    <br>
+    <input type="submit" class="botao">
+    <br>
+    <br>
+    <input type="submit" class="botao">
+        
 
         </div>
     </div>
