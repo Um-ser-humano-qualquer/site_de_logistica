8511ef6600389aac931318c4e7f40315ac400dee
Ruthy Hellen Dias <142604700+R2618@users.noreply.github.com>
2024-07-02 11:27:03 -0300

    finalizado

--- pagina1Prof.php
+++ pagina1Prof.php
@@ -5,11 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Página Inicial</title>
         <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/StylepgC.css">
+        <link rel="stylesheet" href="css/stylepg1Prof.css">
         <link rel="shortcut icon" href="img/icone_creati.ico" type="image/x-icon">
     </head>
         
-    <body class="fundo">
+    <body class="fundinho">
 
         <?php
             include 'menuProf.php'
