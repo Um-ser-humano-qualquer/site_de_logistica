46dbb51e4674eac0db13d43f276ca66a29e3d749
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-07-03 08:40:48 -0300

    sim

--- pagina7Aluno.php
+++ pagina7Aluno.php
@@ -4,7 +4,6 @@
     <link rel="stylesheet" href="css/styleDoBotaoGeral.css">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="css/stylepgG.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="shortcut icon" href="img/icone_creati.ico" type="image/x-icon">
     <title>Movimentação2</title>
