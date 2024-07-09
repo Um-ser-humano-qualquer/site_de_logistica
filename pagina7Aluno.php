020f1bdb2ee2cec4998c32991bd04845fd41d1d9
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-07-09 08:12:31 -0300

    sim

--- pagina7Aluno.php
+++ pagina7Aluno.php
@@ -1,99 +1,11 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<!DOCTYPE html>
 <html lang="pt-br">
     <head>
         <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="css/stylepg6Aluno.css">
-        <link rel="stylesheet" href="css/styleFundo.css">
         <link rel="stylesheet" href="css/stylepgG.css">
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         <link rel="shortcut icon" href="img/icone_creati.ico" type="image/x-icon">
         <title>Movimentação2</title>
     </head>
-
-    <body class="fundo">
-        <div class="divMain2 ">
-            <div class="div-mid">
-                <div class="texto">
-                    <input type="text" size="20">
-                    <br><br>
-                    <input type="text" size="20">
-                </div>
-            
-                <div class="tabela">
-                    
-                        <div class="Btable">
-                            <div class="posicaoTabela">
-                                
-                                    <input type="text" class="tableL texto" placeholder="QNT">
-                                
-                                
-                                    <input type="text" class="tableC texto" placeholder="R$/UNIT">
-                                
-                                
-                                    <input type="text" class="tableR texto" placeholder="POSIÇÃO">
-                                
-                            </div>
-                        </div>
-                    
-
-                    
-        <div class="Line">
-        
-                        
-        </div>
-                        <div class="Btable2">
-                            <div class="posicaoTabela2">
-                                
-                                    <input type="text" class="tableL texto" placeholder="QNT">
-                                
-                                
-                                    <input type="text" class="tableC texto" placeholder="R$/UNIT">
-                                                      
-                                    <input type="text" class="tableR texto" placeholder="POSIÇÃO">                
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="div-ok">
-            <br>
-            <br>
-                <h3>Finalizar</h3>
-                <br>
-                <br>
-                <br>
-                <input type="submit" value="OK" class="botao">
-                <br>
-                <input type="submit" value="OK" class="botao">
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <input type="submit" value="voltar" class="botao">
-            </div>
-        </div>
-        <?php
-                include 'menuAluno.php'
-            ?>
-<<<<<<< HEAD
-=======
-
->>>>>>> 5c0d63047fc60bf54f6f69aaa99092a5db9df19a
->>>>>>> dd9ee33c314bdcb4c0b421718e34c83a26baba81
-<html lang="pt-br"><head>
-    <link rel="stylesheet" href="css/styleDoBotaoGeral.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="css/stylepgG.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="shortcut icon" href="img/icone_creati.ico" type="image/x-icon">
-    <title>Movimentação2</title>
-</head>
 
 <body class="fundo">
     <div class="divMain2 ">
