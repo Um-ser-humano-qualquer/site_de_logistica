f1c6c9e7db50ce33a8b171d9f9ded287481f70f3
Ruthy Hellen Dias <142604700+R2618@users.noreply.github.com>
2024-07-09 07:56:31 -0300

    love

--- pagina7Aluno.php
+++ pagina7Aluno.php
@@ -1,88 +1,3 @@
-<<<<<<< HEAD
-=======
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="css/stylepg6Aluno.css">
-        <link rel="stylesheet" href="css/styleFundo.css">
-        <link rel="stylesheet" href="css/stylepgG.css">
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="shortcut icon" href="img/icone_creati.ico" type="image/x-icon">
-        <title>Movimentação2</title>
-    </head>
-
-    <body class="fundo">
-        <div class="divMain2 ">
-            <div class="div-mid">
-                <div class="texto">
-                    <input type="text" size="20">
-                    <br><br>
-                    <input type="text" size="20">
-                </div>
-            
-                <div class="tabela">
-                    
-                        <div class="Btable">
-                            <div class="posicaoTabela">
-                                
-                                    <input type="text" class="tableL texto" placeholder="QNT">
-                                
-                                
-                                    <input type="text" class="tableC texto" placeholder="R$/UNIT">
-                                
-                                
-                                    <input type="text" class="tableR texto" placeholder="POSIÇÃO">
-                                
-                            </div>
-                        </div>
-                    
-
-                    
-        <div class="Line">
-        
-                        
-        </div>
-                        <div class="Btable2">
-                            <div class="posicaoTabela2">
-                                
-                                    <input type="text" class="tableL texto" placeholder="QNT">
-                                
-                                
-                                    <input type="text" class="tableC texto" placeholder="R$/UNIT">
-                                                      
-                                    <input type="text" class="tableR texto" placeholder="POSIÇÃO">                
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="div-ok">
-            <br>
-            <br>
-                <h3>Finalizar</h3>
-                <br>
-                <br>
-                <br>
-                <input type="submit" value="OK" class="botao">
-                <br>
-                <input type="submit" value="OK" class="botao">
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <input type="submit" value="voltar" class="botao">
-            </div>
-        </div>
-        <?php
-                include 'menuAluno.php'
-            ?>
-<<<<<<< HEAD
-=======
-
->>>>>>> 5c0d63047fc60bf54f6f69aaa99092a5db9df19a
 <html lang="pt-br"><head>
     <link rel="stylesheet" href="css/styleDoBotaoGeral.css">
     <link rel="stylesheet" href="style.css">
@@ -118,11 +33,7 @@
                     </div>
                 
 
-                    <h3>Finalizar</h3>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
+                
     <div class="Line">
        
                     
@@ -151,25 +62,11 @@
             <input type="submit" value="Ok" class="botao">
             </div>
             <br>
-
             <div class="divLoginMy">
             <input type="submit" value="Ok" class="botu">
             </div>
 
             <form method="POST0" action="pagina6Aluno.php">
-
-            <br>
-            <br>
-            <br>
-            <br>
-            <input type="submit" value="OK" class="botao">
-            <br>
-            <input type="submit" value="OK" class="botao">
-            <br>
-            <br>
-            <br>
-            <br>
-
             <input type="submit" value="voltar" class="botao">
             </form>
         </div>
@@ -180,10 +77,5 @@
             include 'menuAluno.php'
     ?>
 
-<<<<<<< HEAD
-=======
-
->>>>>>> e9df01f374cb4c1a83c18ef9b2a42803f557aad6
->>>>>>> 5c0d63047fc60bf54f6f69aaa99092a5db9df19a
     </body>
 </html>
