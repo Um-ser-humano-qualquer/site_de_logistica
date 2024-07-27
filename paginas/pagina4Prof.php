f7b2b3e640b1f12b0d0ead3aac924e5cb0a9c353
Ruthy Hellen Dias <142604700+R2618@users.noreply.github.com>
2024-07-26 23:44:21 -0300

    criação da página4Prof e css página4Prof

--- paginas/pagina4Prof.php
+++ paginas/pagina4Prof.php
@@ -11,6 +11,66 @@
 </head>
 
 <body class="mando">
+    <div class="divMain inline">
+            <div class="divLogin-left">
+                <h1 class="texto">Estudantes</h1>
+                <input type="text" name="Pedido1" id="Pedido1" size="15" class="caixaTexto textoPosicao posicaoPedido1" placeholder="Nome">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido2" size="15" class="caixaTexto textoPosicao" placeholder="Nome">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido3" size="15" class="caixaTexto textoPosicao" placeholder="Nome">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido4" size="15" class="caixaTexto textoPosicao" placeholder="Nome">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido5" size="15" class="caixaTexto textoPosicao" placeholder="Nome">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido6" size="15" class="caixaTexto textoPosicao" placeholder="Nome">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido7" size="15" class="caixaTexto textoPosicao" placeholder="Nome">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido8" size="15" class="caixaTexto textoPosicao" placeholder="Nome">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido9" size="15" class="caixaTexto textoPosicao" placeholder="Nome">
+                <br>
+                <input type="text" name="Pedido1" id="Pedido10" size="15" class="caixaTexto textoPosicao" placeholder="Nome">
+                <br>
+            </div>
+            <div class="divLogin-mid">
+                <h1 class="texto texto-posicao">Código</h1>
+                <div class="divLogin-mid posicao-div-top"></div>
+                <div class="divLogin-mid"></div>
+                <div class="divLogin-mid"></div>
+                <div class="divLogin-mid"></div>
+                <div class="divLogin-mid"></div>
+                <div class="divLogin-mid"></div>
+                <div class="divLogin-mid"></div>
+                <div class="divLogin-mid"></div>
+                <div class="divLogin-mid"></div>
+                <div class="divLogin-mid"></div>
+            </div>   
+            <div class="OK">
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+                <input type="submit" class="btn-OK" value="OK"></input>
+                <br>
+            </div>
+        </div>
 
 <?php
         include 'menuProf.php';
