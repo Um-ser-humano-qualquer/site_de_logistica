ff029343fd53f330791e6c7e6ee050de16013981
Ruthy Hellen Dias <142604700+R2618@users.noreply.github.com>
2024-08-01 09:44:37 -0300

    modificações na tela de recebimento.

--- paginas/pagina6Aluno.php
+++ paginas/pagina6Aluno.php
@@ -5,65 +5,119 @@
         <link rel="stylesheet" href="../css/stylepg6Aluno.css">
         <link rel="stylesheet" href="../css/styleFundo.css">
         <link rel="shortcut icon" href="../img/icone_creati.ico" type="image/x-icon">
-        <title>Movimentação1</title>
+        <title>Movimentação</title>
     </head>
 
     <body class="fundinhoA">
     <div class="divMainX">
+        <div class="divLogin-senhor">
             <div class="divLogin-meu">
-                    <h2 class="texto">Descrição do produto</h2>
+                        <h2 class="texto">Descrição do produto</h2>
                         <br>
-                            <input type="text" name="" size="20" class="caixaTexto" placeholder="">
+                            <input type="text" name="" size="20" class="caixaTexto divLogin-amado" placeholder="">
                         <br/>
                         <br>
-                            <input type="text" name="" size="20" class="caixaTexto" placeholder="">
+                            <input type="text" name="" size="20" class="caixaTexto divLogin-amado" placeholder="">
                         <br/>
                         <br>
-                            <input type="text" name="" size="20" class="caixaTexto" placeholder="">
+                            <input type="text" name="" size="20" class="caixaTexto divLogin-amado" placeholder="">
                         <br/>
                         <br>
-                            <input type="text" name="" size="20" class="caixaTexto" placeholder="">
+                            <input type="text" name="" size="20" class="caixaTexto divLogin-amado" placeholder="">
                         <br/>
                         <br>
-                            <input type="text" name="" size="20" class="caixaTexto" placeholder="">
+                            <input type="text" name="" size="20" class="caixaTexto divLogin-amado" placeholder="">
                         <br/>
                         <br>
-                            <input type="text" name="" size="20" class="caixaTexto" placeholder="">
+                            <input type="text" name="" size="20" class="caixaTexto divLogin-amado" placeholder="">
                         <br/>
-                        <br>
-                            <input type="text" name="" size="20" class="caixaTexto" placeholder="">
-                        <br/>
-            
-        <div class= "Newtable">
-            <div class="Btable"> <!-- primeira tabela -->
-                    <div class="posicaoTabela">
-                        
-                            <input type="text" class="tableL texto" placeholder="UN">
-                        
-                            <input type="text" class="tableC texto" placeholder="QNT">
-
-                            <input type="text" class="tableD texto" placeholder="R$/UNIT">
-                        
-                            <input type="text" class="tableR texto" placeholder="Total">     
-                    </div>
-            </div>
-
-            <div class="Btable"> <!-- primeira tabela -->
-                    <div class="posicaoTabela">
-                        
-                            <input type="text" class="tableL texto" placeholder="UN">
-                        
-                            <input type="text" class="tableC texto" placeholder="QNT">
-
-                            <input type="text" class="tableD texto" placeholder="R$/UNIT">
-                        
-                            <input type="text" class="tableR texto" placeholder="Total">     
-                    </div>
-            </div>
-       </div>
-
         </div>
-    </div>         
+
+            <div class="Btable"> <!-- primeira tabela -->
+                    <div class="posicaoTabela">
+                        
+                            <input type="text" class="tableA texto" placeholder="UN">
+                        
+                            <input type="text" class="tableB texto" placeholder="QNT">
+
+                            <input type="text" class="tableC texto" placeholder="R$/UNIT">
+                        
+                            <input type="text" class="tableD texto" placeholder="Total">     
+                    </div>
+            </div>
+
+            <div class="Btable2"> <!-- segunda tabela -->
+                    <div class="posicaoTabela2">
+                        
+                            <input type="text" class="tableE texto" placeholder="UN">
+                        
+                            <input type="text" class="tableF texto" placeholder="QNT">
+
+                            <input type="text" class="tableG texto" placeholder="R$/UNIT">
+                        
+                            <input type="text" class="tableH texto" placeholder="Total">     
+                    </div>
+            </div>
+
+            <div class="Btable3"> <!-- terceira tabela -->
+                    <div class="posicaoTabela3">
+                        
+                            <input type="text" class="tableI texto" placeholder="UN">
+                        
+                            <input type="text" class="tableJ texto" placeholder="QNT">
+
+                            <input type="text" class="tableK texto" placeholder="R$/UNIT">
+                        
+                            <input type="text" class="tableL texto" placeholder="Total">     
+                    </div>
+            </div>
+
+            <div class="Btable4"> <!-- quarta tabela -->
+                    <div class="posicaoTabela4">
+                        
+                            <input type="text" class="tableM texto" placeholder="UN">
+                        
+                            <input type="text" class="tableN texto" placeholder="QNT">
+
+                            <input type="text" class="tableO texto" placeholder="R$/UNIT">
+                        
+                            <input type="text" class="tableP texto" placeholder="Total">     
+                    </div>
+            </div>
+
+            <div class="Btable5"> <!-- quinta tabela -->
+                    <div class="posicaoTabela5">
+                        
+                            <input type="text" class="tableQ texto" placeholder="UN">
+                        
+                            <input type="text" class="tableR texto" placeholder="QNT">
+
+                            <input type="text" class="tableS texto" placeholder="R$/UNIT">
+                        
+                            <input type="text" class="tableT texto" placeholder="Total">     
+                    </div>
+            </div>
+
+            <div class="Btable6"> <!-- sexta tabela -->
+                    <div class="posicaoTabela6">
+                        
+                            <input type="text" class="tableU texto" placeholder="UN">
+                        
+                            <input type="text" class="tableV texto" placeholder="QNT">
+
+                            <input type="text" class="tableW texto" placeholder="R$/UNIT">
+                        
+                            <input type="text" class="tableX texto" placeholder="Total">     
+                    </div>
+            </div>
+
+    </div>
+
+            <div class="divLogin-bot">
+                <input type="submit" value="Gerar Endereço" class="batu">
+            </div>
+
+  </div>         
         
             </div>
             <?php
