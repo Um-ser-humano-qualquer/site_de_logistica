cac48c7aa8381ab8d9283259d3ee82766889e7a7
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-08-01 09:24:46 -0300

    para de fazer isso oooo, seu restinho de gozo

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,99 @@
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-  }
+function popupA1(){
+  var popup = document.getElementById("myPopupA1");
+  popup.classList.toggle("showA1");
+}
+
+function popupA2(){
+  var popup = document.getElementById("myPopupA2");
+  popup.classList.toggle("showA2");
+}
+
+function popupA3(){
+  var popup = document.getElementById("myPopupA3");
+  popup.classList.toggle("showA3");
+}
+
+function popupA4(){
+  var popup = document.getElementById("myPopupA4");
+  popup.classList.toggle("showA4");
+}
+
+function popupA5(){
+  var popup = document.getElementById("myPopupA5");
+  popup.classList.toggle("showA5");
+}
+
+function popupB1(){
+  var popup = document.getElementById("myPopupB1");
+  popup.classList.toggle("showB1");
+}
+
+function popupB2(){
+  var popup = document.getElementById("myPopupB2");
+  popup.classList.toggle("showB2");
+}
+
+function popupB3(){
+  var popup = document.getElementById("myPopupB3");
+  popup.classList.toggle("showB3");
+}
+
+function popupB4(){
+  var popup = document.getElementById("myPopupB4");
+  popup.classList.toggle("showB4");
+}
+
+function popupB5(){
+  var popup = document.getElementById("myPopupB5");
+  popup.classList.toggle("showB5");
+}
+
+function popupC1(){
+  var popup = document.getElementById("myPopupC1");
+  popup.classList.toggle("showC1");
+}
+
+function popupC2(){
+  var popup = document.getElementById("myPopupC2");
+  popup.classList.toggle("showC2");
+}
+
+function popupC3(){
+  var popup = document.getElementById("myPopupC3");
+  popup.classList.toggle("showC3");
+}
+
+function popupC4(){
+  var popup = document.getElementById("myPopupC4");
+  popup.classList.toggle("showC4");
+}
+
+function popupC5(){
+  var popup = document.getElementById("myPopupC5");
+  popup.classList.toggle("showC5");
+}
+
+function popupD1(){
+  var popup = document.getElementById("myPopupD1");
+  popup.classList.toggle("showD1");
+}
+
+function popupD2(){
+  var popup = document.getElementById("myPopupD2");
+  popup.classList.toggle("showD2");
+}
+
+function popupD3(){
+  var popup = document.getElementById("myPopupD3");
+  popup.classList.toggle("showD3");
+}
+
+function popupD4(){
+  var popup = document.getElementById("myPopupD4");
+  popup.classList.toggle("showD4");
+}
+
+function popupD5(){
+  var popup = document.getElementById("myPopupD5");
+  popup.classList.toggle("showD5");
+}
--- paginas/pagina8Aluno.php
+++ paginas/pagina8Aluno.php
@@ -25,136 +25,135 @@
         <table class="divEstoque2">
             <tr>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupA1 btn-estoque" onclick="popupA1()">
                         <p class="botaoPosicao texto">A1</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextA1" id="myPopupA1">exemplo</span>
                     </div>
                 </th>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupA2 btn-estoque" onclick="popupA2()">
                         <p class="botaoPosicao texto">A2</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextA2" id="myPopupA2">exemplo</span>
                     </div>
                 </th>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupA3 btn-estoque" onclick="popupA3()">
                         <p class="botaoPosicao texto">A3</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextA3" id="myPopupA3">exemplo</span>
                     </div>
                 </th>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupA4 btn-estoque" onclick="popupA4()">
                         <p class="botaoPosicao texto">A4</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextA4" id="myPopupA4">exemplo</span>
+                    </div>
+                </th>
+                <th>
+                    <div class="popupA5 btn-estoque" onclick="popupA5()">
+                        <p class="botaoPosicao texto">A5</p>
+                        <span type="submit" class="popuptextA5" id="myPopupA5">exemplo</span>
                     </div>
                 </th>
             </tr>
             <tr>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupB1 btn-estoque" onclick="popupB1()">
                         <p class="botaoPosicao texto">B1</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextB1" id="myPopupB1">exemplo</span>
                     </div>
                 </th>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupB2 btn-estoque" onclick="popupB2()">
                         <p class="botaoPosicao texto">B2</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextB2" id="myPopupB2">exemplo</span>
                     </div>
                 </th>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupB3 btn-estoque" onclick="popupB3()">
                         <p class="botaoPosicao texto">B3</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextB3" id="myPopupB3">exemplo</span>
                     </div>
                 </th>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupB4 btn-estoque" onclick="popupB4()">
                         <p class="botaoPosicao texto">B4</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextB4" id="myPopupB4">exemplo</span>
+                    </div>
+                </th>
+                <th>
+                    <div class="popupB5 btn-estoque" onclick="popupB5()">
+                        <p class="botaoPosicao texto">B5</p>
+                        <span type="submit" class="popuptextB5" id="myPopupB5">exemplo</span>
                     </div>
                 </th>
             </tr>
             <tr>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupC1 btn-estoque" onclick="popupC1()">
                         <p class="botaoPosicao texto">C1</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextC1" id="myPopupC1">exemplo</span>
                     </div>
                 </th>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupC2 btn-estoque" onclick="popupC2()">
                         <p class="botaoPosicao texto">C2</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextC2" id="myPopupC2">exemplo</span>
                     </div>
                 </th>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupC3 btn-estoque" onclick="popupC3()">
                         <p class="botaoPosicao texto">C3</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextC3" id="myPopupC3">exemplo</span>
                     </div>
                 </th>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupC4 btn-estoque" onclick="popupC4()">
                         <p class="botaoPosicao texto">C4</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextC4" id="myPopupC4">exemplo</span>
+                    </div>
+                </th>
+                <th>
+                    <div class="popupC5 btn-estoque" onclick="popupC5()">
+                        <p class="botaoPosicao texto">C5</p>
+                        <span type="submit" class="popuptextC5" id="myPopupC5">exemplo</span>
                     </div>
                 </th>
             </tr>
             <tr>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupD1 btn-estoque" onclick="popupD1()">
                         <p class="botaoPosicao texto">D1</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextD1" id="myPopupD1">exemplo</span>
                     </div>
                 </th>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupD2 btn-estoque" onclick="popupD2()">
                         <p class="botaoPosicao texto">D2</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextD2" id="myPopupD2">exemplo</span>
                     </div>
                 </th>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupD3 btn-estoque" onclick="popupD3()">
                         <p class="botaoPosicao texto">D3</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextD3" id="myPopupD3">exemplo</span>
                     </div>
                 </th>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
+                    <div class="popupD4 btn-estoque" onclick="popupD4()">
                         <p class="botaoPosicao texto">D4</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
-                    </div>
-                </th>
-            </tr>
-            <tr>
-                <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
-                        <p class="botaoPosicao texto">E1</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                        <span type="submit" class="popuptextD4" id="myPopupD4">exemplo</span>
                     </div>
                 </th>
                 <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
-                        <p class="botaoPosicao texto">E2</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
-                        <p class="botaoPosicao texto">E3</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="popup btn-estoque" onclick="myFunction()">
-                        <p class="botaoPosicao texto">E4</p>
-                        <span type="submit" value="A1" class="popuptext" id="myPopup">exemplo</span>
+                    <div class="popupD5 btn-estoque" onclick="popupD5()">
+                        <p class="botaoPosicao texto">D5</p>
+                        <span type="submit" class="popuptextD5" id="myPopupD5">exemplo</span>
                     </div>
                 </th>
             </tr>
         </table>
     </div>
+    <input type="submit" value="Gerar endereço" class="botao botao-posicao">
 </div>
 <?php 
     include 'menuAluno.php'
