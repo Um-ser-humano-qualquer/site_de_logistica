3ddf65dca39cfcfbd28a0e2939221f30b05d069e
Ruthy Hellen Dias <142604700+R2618@users.noreply.github.com>
2024-08-07 21:49:15 -0300

    criação da ágina do romaneio e alterações nas páginas 14Aluno e no menu.

---
+++ paginas/pagina20Aluno.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/stylepg20Aluno.css">
+        <link rel="stylesheet" href="../css/styleFundo.css">
+        <link rel="shortcut icon" href="../img/icone_creati.ico" type="image/x-icon">
+        <title>Página Inicial</title>
+    </head>
+    
+    <body class="fando">
+        <div class="divMain">
+
+        <div class="divLoginP">
+             <input type="text" name="" size="24" class="caixaTexto divLogin-baby" placeholder=" Links">
+
+            <div class="divLogin-left">
+                <textarea id="story" name="story" rows="5" cols="33" placeholder="Adendos"></textarea>
+            </div>
+
+            <div class="divLogin-let">
+                <div class="help"></div>
+            </div>
+        </div>
+
+    </div>
+
+        <?php
+            include 'menuAluno.php'
+        ?>
+    </body>
+</html>
