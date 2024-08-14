531c622ae405721a80bc01898c821ecc079899f5
Ruthy Hellen Dias <142604700+R2618@users.noreply.github.com>
2024-08-14 08:45:54 -0300

    faltam 6 dias para apresentação

--- paginas/pagina6Prof.php
+++ paginas/pagina6Prof.php
@@ -4,20 +4,20 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../css/style.css">
-        <link rel="stylesheet" href="../css/stylepg20Aluno.css">
+        <link rel="stylesheet" href="../css/stylepg6Prof.css">
         <link rel="stylesheet" href="../css/styleFundo.css">
         <link rel="shortcut icon" href="../img/icone_creati.ico" type="image/x-icon">
-        <title>Página Inicial</title>
+        <title>Romaneio</title>
     </head>
-    
-    <body class="fando">
+   
+    <body class="fundinho">
         <div class="divMain">
 
         <div class="divLoginP">
-             <input type="text" name="" size="24" class="caixaTexto divLogin-baby" placeholder=" Links">
+             <input type="text" name="" size="24" class="caixaTexto divLogin-baby" placeholder=" Email: ">
 
             <div class="divLogin-left">
-                <textarea id="story" name="story" rows="5" cols="33" placeholder="Adendos"></textarea>
+                <textarea id="story" name="story" rows="8" cols="34" placeholder="Observações"></textarea>
             </div>
 
             <div class="divLogin-let">
@@ -26,9 +26,12 @@
         </div>
         <input type="submit" value="Enviar" class="bot batu">
     </div>
+    
+
 
         <?php
-            include 'menuAluno.php'
+            include 'menuProf.php'
         ?>
     </body>
+
 </html>
