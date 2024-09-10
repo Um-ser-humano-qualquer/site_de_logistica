c5c03ffe2d5cfe44ff9036a1c175db03a2e30b02
André Santiago dos Santos <142604035+Um-ser-humano-qualquer@users.noreply.github.com>
2024-09-10 12:38:25 -0300

    salvar pra depois

--- php/guarda_id.php
+++ php/guarda_id.php
@@ -5,15 +5,59 @@ $id_pedido2 = $connect->real_escape_string($_POST["Pedido2"]);
 $id_pedido3 = $connect->real_escape_string($_POST["Pedido3"]);
 $id_pedido4 = $connect->real_escape_string($_POST["Pedido4"]);
 
-$id_pedido = array($id_pedido1, $id_pedido2,$id_pedido3, $id_pedido4);
+if ($id_pedido1 != 0){
 
-$sql = "INSERT INTO `guardar_id`(`id_guardado`) VALUES ('$id_pedido1'), ('$id_pedido2'), ('$id_pedido3'), ('$id_pedido4')";
+    $id_pedido = array($id_pedido1);
+    
+    $sql = "INSERT INTO `guardar_id`(`id_guardado`) VALUES ('$id_pedido1')";
+    
+    $connect->close();
 
+    header('location: ../paginas/Pagina13Aluno.php', true, 301);
+    exit();
 
-$resultado = $connect->query($sql);
+}elseif ($id_pedido2 != 0){
+    
+    $id_pedido = array($id_pedido2);
+    
+    $sql = "INSERT INTO `guardar_id`(`id_guardado`) VALUES ('$id_pedido2')";
+    
+    $connect->close();
 
-$connect->close();
+    header('location: ../paginas/Pagina13Aluno.php', true, 301);
+    exit();
 
-header('location: ../paginas/Pagina13Aluno.php', true, 301);
-exit();
+}elseif ($id_pedido3 != 0){
+    
+    $id_pedido = array($id_pedido3);
+    
+    $sql = "INSERT INTO `guardar_id`(`id_guardado`) VALUES ('$id_pedido3')";
+    
+    $connect->close();
 
+    header('location: ../paginas/Pagina13Aluno.php', true, 301);
+    exit();
+
+}elseif ($id_pedido4 != 0){
+    
+    $id_pedido = array($id_pedido4);
+    
+    $sql = "INSERT INTO `guardar_id`(`id_guardado`) VALUES ('$id_pedido4')";
+    
+    $connect->close();
+
+    header('location: ../paginas/Pagina13Aluno.php', true, 301);
+    exit();
+
+}elseif ($id_pedido1 != 0 && $id_pedido2 = 0 && $id_pedido3 != 0 && $id_pedido4 != 0){
+    
+    $id_pedido = array($id_pedido1, $id_pedido3, $id_pedido4);
+    
+    $sql = "INSERT INTO `guardar_id`(`id_guardado`) VALUES ('$id_pedido1'), ('$id_pedido3'), ('$id_pedido4')";
+    
+    $connect->close();
+
+    header('location: ../paginas/Pagina13Aluno.php', true, 301);
+    exit();
+
+}
